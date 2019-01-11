1490674872d20f384a77531d4344a9399bb6b488
Rajesh S <rajesha.s@gmail.com>
2019-01-11 12:37:23 +0530

    Added the jwt token in the promise data

--- src/client/auth/AuthAdapter.js
+++ src/client/auth/AuthAdapter.js
@@ -6,48 +6,31 @@ For more information about authentication adapters, visit:
 */
 const loginAdapter = (core, config) => ({
   login: (req, res) => {
-    
+
     const username = req.username;
-    
+
     var reqData = new FormData();
-    reqData.append("username",req.username);
-    reqData.append("password",req.password);
-    
+    reqData.append("username", req.username);
+    reqData.append("password", req.password);
+
 
     var request = new XMLHttpRequest();
     console.log("login call")
-    request.onreadystatechange = function(){
-    	if(this.readyState == 4 && this.status == 200){
-    		const resp = JSON.parse(this.responseText);
-    		if(resp["status"] == "success"){
-    			return Promise.resolve({id: 666, username, groups: ['admin']});
-    		}
-    	}
-    };
+    
     // call to login API
-    request.open('POST','http://jenkins.oxzion.com:8080/auth',false);
+    request.open('POST', 'http://jenkins.oxzion.com:8080/auth', false);
     request.send(reqData);
     if (request.status === 200) {
-  		console.log(request.responseText);
-    	return Promise.resolve({id: 666, username, groups: ['admin']});
-	}
-
-
-    /*
-		var data = new FormData();
-    data.append('username',req.username);
-    data.append('password',req.password);
-    const resp = fetch('http://jenkins.oxzion.com:8080/auth',{
-    	method: 'POST',
-    	body: data
-    });
-	const respData = await resp.json()
-    console.log(respData);
-
-    return Promise.resolve({id: 666, username, groups: ['admin']});
-      if (req.username === 'test' && req.password === 'demo') {
-      return Promise.resolve({id: 666, username, groups: ['admin']});
-    }*/
+      const resp = JSON.parse(request.responseText);
+      console.log(resp);
+      return Promise.resolve({
+        id: 666,
+        username,
+        groups: ['admin'],
+        token: resp.data.jwt
+      });
+    }
+   
   },
 
   logout: (req, res) => {
