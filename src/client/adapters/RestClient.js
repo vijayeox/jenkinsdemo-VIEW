c8a6b7697bbe132ceff94f9a53f2b95bfffdc40b
sarin <sarin@myvamla.com>
2019-02-08 19:27:06 +0530

    Email Contact List CRUD API Implementation V2

--- src/client/adapters/RestClient.js
+++ src/client/adapters/RestClient.js
@@ -1,10 +1,10 @@
-import {ServiceProvider} from '@osjs/common';
+import { ServiceProvider } from '@osjs/common';
 
 
 export class RestClientServiceProvider extends ServiceProvider {
 
 	constructor(core, options = {}) {
-	    super(core, options || {});
+		super(core, options || {});
 		this.core = core;
 		this.token = null;
 		this.baseUrl = this.core.config('wrapper.url');
@@ -14,13 +14,13 @@ export class RestClientServiceProvider extends ServiceProvider {
 
 	providers() {
 		return [
-		'oxzion/restClient'
+			'oxzion/restClient'
 		];
 	}
 
 	async init() {
 		this.core.instance('oxzion/restClient', () => ({
-			request: (version,action,params,method) => this.makeRequest(version,action,params,method),
+			request: (version, action, params, method) => this.makeRequest(version, action, params, method),
 			authenticate: (params) => this.authenticate(params)
 		}));
 
@@ -30,7 +30,7 @@ export class RestClientServiceProvider extends ServiceProvider {
 	// auth wrapper 
 	async authenticate(params) {
 		try {
-			var respData ;
+			var respData;
 			let url = this.baseUrl + 'auth';
 			//console.log(url);
 			const testURL = this.core.config('auth.url');
@@ -40,7 +40,7 @@ export class RestClientServiceProvider extends ServiceProvider {
 			})
 			return resp.json();
 		}
-		catch (e) {}
+		catch (e) { }
 	}
 
 
@@ -49,82 +49,86 @@ export class RestClientServiceProvider extends ServiceProvider {
 	// action - string
 	// params - *
 	// method - string
-	async makeRequest(version,action,params,method) {
-		let userData =  this.core.getUser();
-		if(action.charAt(0)== '/')
+	async makeRequest(version, action, params, method) {
+		let userData = this.core.getUser();
+		if (action.charAt(0) == '/')
 			action = action.substr(1);
 		let urlString = this.baseUrl + action;
-		//console.log(urlString);
+		console.log(urlString);
 		this.token = userData["jwt"];
 		try {
-			
-			if(method == 'get')  {
+
+			if (method == 'get') {
 				let auth = 'Bearer ' + this.token;
-				const resp = await fetch(urlString,{
+				const resp = await fetch(urlString, {
 					method: method,
-					credentials:'include',
+					credentials: 'include',
 					headers: {
-				      'Authorization': auth,
-	      			  'Content-Type': 'application/json'
-				    }
-					
+						'Authorization': auth,
+						'Content-Type': 'application/json'
+					}
+
 				})
 
 				return resp.json();
 			}
-			else if(method == 'post') {
+			else if (method == 'post') {
 				let auth = 'Bearer ' + this.token;
-				const resp = await fetch(urlString,{
-					method: method,
-					credentials:'include',
-					headers: {
-					  'Authorization': auth,
-					  'Content-Type': 'application/json'
-				    },
-					body: params.data
-				})	
-
-				return resp.json();
-			}
-			else if(method == 'put') {
-				let jsonObject = {};
-				for (const [key, value]  of params.data.entries()) {
-    				jsonObject[key] = value;
+				let parameters = params.data;
+				if (typeof parameters === 'string') {
+					parameters = JSON.parse(parameters)
 				}
-				console.log(jsonObject)
-				let auth = 'Bearer ' + this.token;
-				console.log(params.data)
-				const resp = await fetch(urlString,{
+				let formData = new FormData();
+				for (var k in parameters) {
+					formData.append(k, parameters[k]);
+				}
+				const resp = await fetch(urlString, {
 					method: method,
-					credentials:'include',
-					headers: new Headers({
-				      'Authorization': auth,
-				      'Content-Type':'application/json'
-				    }),
-					body: JSON.stringify(jsonObject)
-				})	
-
-				return resp.json();
-			}
-			else if(method == 'delete') {
-				let auth = 'Bearer ' + this.token;
-				const resp = await fetch(urlString,{
-					method: method,
-					credentials:'include',
-					headers: new Headers({
-				      'Authorization': auth,
-				    }),
-					
+					credentials: 'include',
+					headers: {
+						'Authorization': auth
+					},
+					body: formData
 				})
 
 				return resp.json();
 			}
-			else
-			{
+			else if (method == 'put') {
+				let parameters = params.data;
+				if (typeof parameters === 'string') {
+					parameters = JSON.parse(parameters)
+				}
+				let auth = 'Bearer ' + this.token;
+				const resp = await fetch(urlString, {
+					method: method,
+					credentials: 'include',
+					headers: new Headers({
+						'Authorization': auth,
+						'Content-type': 'application/json'
+					}),
+					body: JSON.stringify(parameters)
+				})
+
+				return resp.json();
+			}
+			else if (method == 'delete') {
+				let auth = 'Bearer ' + this.token;
+				const resp = await fetch(urlString, {
+					method: method,
+					credentials: 'include',
+					headers: new Headers({
+						'Authorization': auth,
+					}),
+
+				})
+
+				return resp.json();
+			}
+			else {
 				console.log('Unsupported method.');
 			}
 			// TODO - handle refresh
-			return null;			
+			return null;
 		}
 		catch (e) {
 			return Promise.reject(e);
