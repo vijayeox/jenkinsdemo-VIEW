eacf9a07965071dead5b261a4fcd53cdf49f3ce3
neha <neha@myvamla.com>
2019-02-25 13:48:20 +0530

    Used materialize within the application namespace.

--- apps/Announcements/index.js
+++ apps/Announcements/index.js
@@ -13,52 +13,52 @@ const register = (core, args, options, metadata) => {
 	// Create a new Application instance
 	const proc = core.make('osjs/application', { args, options, metadata });
 
-	// Create  a new Window instance
-	const win = proc => {
-		proc.createWindow({
-			id: 'annoucementsWindow',
-			title: metadata.title.en_EN,
-			icon: proc.resource(icon),
-			dimension: { width: 700, height: 400 },
-			position: { left: 10, top: 60 },
-			attributes: {
-				visibility: 'restricted',
-			},
-		}).render($content => ReactDOM.render(<Slider args={core} />, $content));
-	};
-
-	console;
-	const getAnnouncements = async () => {
-		let helper = core.make('oxzion/restClient');
-		let announ = await helper.request('v1', '/announcement', {}, 'get');
-		console.log(announ);
-		return announ;
-	};
-
-	let announcementsCount = 0;
-	getAnnouncements().then(response => {
-		console.log(response);
-		announcementsCount = response['data'].length;
-
-		if (core.has('osjs/tray')) {
-			const tray = core.make('osjs/tray').create(
-				{
-					icon: proc.resource(metadata.icon),
-					badge: 'badgeCheck',
-					count: announcementsCount,
-				},
-				ev => {
-					core.make('osjs/contextmenu').show({
-						position: ev,
-						menu: [{ label: 'Show', onclick: () => win(proc) }],
-					});
-				}
-			);
-		}
-	});
-
+  // Create  a new Window instance
+    const win = (proc) => {
+      const window = proc.createWindow({
+        id: 'annoucementsWindow',
+        title: metadata.title.en_EN,
+        icon: proc.resource(icon),
+        dimension: {width: 700, height: 400},
+        position: {left: 700, top: 200}, 
+        attributes : {
+          visibility: 'restricted'
+        }
+      }).render($content => ReactDOM.render(<Slider  args = {core} />, $content));
+      if(window.$element.className.indexOf('Window_'+applicationName) == -1){
+        window.$element.className += " Window_"+applicationName;
+      }
+    }
+    
+    const getAnnouncements = async () => {
+        let helper = core.make('oxzion/restClient');
+        let announ = await helper.request('v1','/announcement', {}, 'get' );
+        console.log(announ);
+        return announ;
+    };
+    
+    let announcementsCount=0 ;
+    getAnnouncements().then(response => {
+      console.log(response);
+      announcementsCount = response["data"].length;
+      
+      if (core.has('osjs/tray')) {
+        const tray = core.make('osjs/tray').create({
+          icon: proc.resource(metadata.icon),
+          badge: 'badgeCheck',
+          count: announcementsCount
+        }, (ev) => {
+          core.make('osjs/contextmenu').show({
+            position: ev,
+            menu: [
+              {label: 'Show', onclick: () => win(proc)}
+            ]
+          });
+        });
+      }
+    
+    })
 	win(proc);
-
 	// Creates a new WebSocket connection (see server.js)
 	//const sock = proc.socket('/socket');
 	//sock.on('message', (...args) => console.log(args))
