d0bc2ed76e6cbacd43ae8b83673c542462859545
sarin <sarin@myvamla.com>
2019-03-14 15:46:32 +0530

    Email notification implementation

--- apps/OXMail/index.js
+++ apps/OXMail/index.js
@@ -1,154 +1,222 @@
-/*
- * OS.js - JavaScript Cloud/Web Desktop Platform
- *
- * Copyright (c) 2011-2018, Anders Evenrud <andersevenrud@gmail.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * @author  Anders Evenrud <andersevenrud@gmail.com>
- * @licence Simplified BSD License
- */
-import { name as applicationName, mailServer as baseUrl } from "./metadata.json";
+  /*
+  * OS.js - JavaScript Cloud/Web Desktop Platform
+  *
+  * Copyright (c) 2011-2018, Anders Evenrud <andersevenrud@gmail.com>
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without
+  * modification, are permitted provided that the following conditions are met:
+  *
+  * 1. Redistributions of source code must retain the above copyright notice, this
+  *    list of conditions and the following disclaimer
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  * @author  Anders Evenrud <andersevenrud@gmail.com>
+  * @licence Simplified BSD License
+  */
+  import {
+    name as applicationName,
+    mailServer as baseUrl
+  } from "./metadata.json";
+  const trayOptions = {};
+  let mailCount = 0;
+  let tray = null;
 
-const createIframe = (bus, proc, win, cb) => {
-  const iframe = document.createElement("iframe");
-  iframe.style.width = "100%";
-  iframe.style.height = "100%";
-  iframe.setAttribute("border", "0");
-
-  iframe.addEventListener("load", () => {
-    const ref = iframe.contentWindow;
-    // win.maximize(); // Maximize
-
-    // This will proxy the window focus events to iframe
-    win.on("focus", () => ref.focus());
-    win.on("blur", () => ref.blur());
-
-    // Create message sending wrapper
-    const sendMessage = msg => ref.postMessage(msg, baseUrl);
-
-    // After connection is established, this handler will process
-    // all events coming from iframe.
-    proc.on("message", data => {
-      console.warn("[Application", "Iframe sent", data);
-      bus.emit(data.method, sendMessage, ...data.args);
-    });
-    cb(sendMessage);
-  });
-
-  return iframe;
-};
-const makeApiCall = function(core, params) {
-  return (async () => {
-    var caller = core.make("oxzion/restClient");
-    // console.log(params.data)
-    var res = await caller.request(
-      params.version,
-      params.action,
-      params.data,
-      params.method
-    );
-    if (res != null && res.status == "success") {
-      return Promise.resolve(res);
-    } else if (res != null && res.status != "success") {
-      console.log("login failed.");
-      return Promise.reject(new Error(res.message));
-    } else {
-      return Promise.reject(new Error(res.message));
+  const resetBadge = () => {
+    if (trayOptions.count > 0) {
+      trayOptions.count = undefined;
+      trayOptions.badge = "";
+      tray.update(trayOptions);
     }
-  })();
-};
-// Creates the internal callback function when OS.js launches an application
-// Note the first argument is the 'name' taken from your metadata.json file
-OSjs.make("osjs/packages").register(
-  "OXMail",
-  (core, args, options, metadata) => {
-    // Create a new Application instance
-    const proc = core.make("osjs/application", {
-      args,
-      options,
-      metadata
+  };
+
+  const createIframe = (proc, win) => {
+    const iframe = document.createElement("iframe");
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.setAttribute("border", "0");
+
+    iframe.addEventListener("load", () => {
+      const ref = iframe.contentWindow;
+      // This will proxy the window focus events to iframe
+      win.on("focus", () => ref.focus());
+      win.on("blur", () => ref.blur());
+      // Create message sending wrapper
+      const sendMessage = msg => ref.postMessage(msg, baseUrl);
+      // After connection is established, this handler will process
+      // all events coming from iframe.
+      proc.on("message", data => {
+        console.warn("[Application", "Iframe sent", data);
+        bus.emit(data.method, sendMessage, ...data.args);
+      });
     });
 
-    // Create  a new Window instance
-    const win = proc
-      .createWindow({
-        id: "OXMailApplicationWindow",
-        icon: proc.resource(proc.metadata.icon),
-        title: metadata.title.en_EN,
-        attributes: { state: { maximized: true } }
-        // dimension: {width: 400, height: 400},
-        // position: {left: 200, top: 0}
-      })
-      .on("destroy", () => proc.destroy())
-      // .on('init', () => ref.maximize())
-      .render(($content, win) => {
-        win.maximize();
-        win.attributes.maximizable = false;
-        // Create a new bus for our messaging
-        const bus = core.make("osjs/event-handler", "OXMailApplicationWindow");
-        const user = core.make("osjs/auth").user();
-        // Get path to iframe content
-        const src = proc.resource(
-          baseUrl + "/oxindex.php?username=" + user.username
-        );
+    return iframe;
+  };
 
-        // Create DOM element
-        const iframe = createIframe(bus, proc, win, send => {
-          bus.on("yo", (send, args) =>
-            send({
-              method: "yo",
-              args: ["OX Mail says hello"]
-            })
-          );
-
-          // Send the process ID to our iframe to establish communication
-          send({
-            method: "init",
-            args: [proc.pid]
-          });
-        });
-
-        // Finally set the source and attach
-        iframe.src = src;
-        bus.on("contact", (callback, params) => {
-          // console.log(params);
-          makeApiCall(core, params)
-            .then(res => {
-              callback({ method: "contact", params: params, data: res.data });
-            })
-            .catch(err => {
-              callback({
-                method: "contact",
-                params: params,
-                error: err.message
-              });
-            });
-        });
-
-        // Attach
-        $content.appendChild(iframe);
+  const makeApiCall = function(core, params) {
+    return (async () => {
+      var caller = core.make("oxzion/restClient");
+      // console.log(params.data)
+      var res = await caller.request(
+        params.version,
+        params.action,
+        params.data,
+        params.method
+      );
+      if (res != null && res.status == "success") {
+        return Promise.resolve(res);
+      } else if (res != null && res.status != "success") {
+        console.log("login failed.");
+        return Promise.reject(new Error(res.message));
+      } else {
+        return Promise.reject(new Error(res.message));
+      }
+    })();
+  };
+  // Creates the internal callback function when OS.js launches an application
+  // Note the first argument is the 'name' taken from your metadata.json file
+  OSjs.make("osjs/packages").register(
+    "OXMail",
+    (core, args, options, metadata) => {
+      // Create a new Application instance
+      const proc = core.make("osjs/application", {
+        args,
+        options,
+        metadata
       });
 
-    return proc;
-  }
-);
+      let trayInitialized = false;
+      // Create  a new Window instance
+      const createProcWindow = () => {
+        const win = proc
+          .createWindow({
+            id: "OXMailApplicationWindow",
+            icon: proc.resource(proc.metadata.icon),
+            title: metadata.title.en_EN,
+            attributes: { state: { maximized: true } },
+            attributes: {
+              visibility: "restricted",
+              closeable: false,
+              minimizable: true
+            }
+          })
+          .on("close", () => {
+            win.state.minimized == true;
+            win.minimize = true;
+            win.attributes.minimized = true;
+            // win.minimize();
+            // win.attributes.minimizable = true;
+            console.log("close event");
+          })
+          .on("destroy", () => proc.destroy())
+          .render(($content, win) => {
+            win.maximize();
+            win.attributes.maximizable = false;
+            // Create a new bus for our messaging
+            const profile = core.make("oxzion/profile");
+            const details = profile.get();
+            const suffix = `?pid=${proc.pid}&wid=${win.wid}`;
+            const src = proc.resource(
+              baseUrl + "/oxindex.php" + suffix + "&email=" + details.key.email
+            );
+            // Create DOM element
+            const iframe = createIframe(proc, win);
+            const handleNotification = () => {
+              if (win.state.focused == false || win.state.minimized == true) {
+                mailCount++;
+                mailCount = mailCount > 0 ? mailCount : "";
+                trayOptions.badge = "badgeCheck";
+                trayOptions.count = mailCount;
+              }
+              tray.update(trayOptions);
+            };
+            // Send the process ID to our iframe to establish communication
+            win.on("blur", () => iframe.contentWindow.blur());
+            win.on("focus", () => iframe.contentWindow.focus());
+            win.on("iframe:post", msg =>
+              iframe.contentWindow.postMessage(msg, src)
+            );
+            win.on("iframe:get", msg => {
+              console.warn("Message from Iframe", msg);
+              if (msg === "Ping") {
+                win.emit("iframe:post", "Pong");
+              } else if (msg instanceof Object && msg.method == "notify") {
+                handleNotification();
+              } else if (
+                msg instanceof Object &&
+                msg.method &&
+                msg.args &&
+                Array.isArray(msg.args)
+              ) {
+                const params = msg.args[0];
+                makeApiCall(core, params)
+                  .then(res => {
+                    win.emit("iframe:post", {
+                      method: msg.method,
+                      params: params,
+                      data: res.data
+                    });
+                  })
+                  .catch(err => {
+                    win.emit("iframe:post", {
+                      method: msg.method,
+                      params: params,
+                      error: err.message
+                    });
+                  });
+              }
+            });
+            // Finally set the source and attach
+            iframe.src = src;
+            // Tray Icon
+            if (core.has("osjs/tray") && !trayInitialized) {
+              trayInitialized = true;
+              trayOptions.title = "Mail";
+              trayOptions.icon = proc.resource(metadata.icon);
+
+              tray = core.make("osjs/tray").create(trayOptions, ev => {
+                core.make("osjs/contextmenu").show({
+                  position: ev,
+                  menu: [
+                    {
+                      label: "Open",
+                      onclick: () => {
+                        console.log(proc);
+                        win.raise();
+                        win.focus();
+                        resetBadge();
+                      }
+                    },
+                    {
+                      label: "Quit",
+                      onclick: () => {
+                        proc.destroy();
+                        tray.destroy();
+                      }
+                    }
+                  ]
+                });
+              });
+            }
+            // Attach
+            $content.appendChild(iframe);
+          });
+      };
+      createProcWindow();
+      return proc;
+    }
+  );
