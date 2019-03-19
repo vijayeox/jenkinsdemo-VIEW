1aaf3d2a69c66d37e06a552e99b03cd6d81f12e9
bharatg <bharatg@myvamla.com>
2019-03-19 22:36:42 +0530

    Indentation and remove unused imports

--- bos/src/client/packages/panels/src/panel.js
+++ bos/src/client/packages/panels/src/panel.js
@@ -30,264 +30,234 @@
 
 import PanelItem from './panel-item';
 import * as languages from './locales';
-import { EventEmitter } from '@osjs/event-emitter';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {EventEmitter} from '@osjs/event-emitter';
 import 'materialize-css/dist/css/materialize.min.css';
-import LocalStorageAdapter from './../../../adapters/localStorageAdapter.js';
 /**
  * Panel
  *
  * @desc Base Panel Class
  */
 export default class Panel extends EventEmitter {
-	/**
-	 * Create panel
-	 *
-	 * @param {Core} core Core reference
-	 * @param {Object} options Options
-	 */
-	constructor(core, options = {}) {
-		super('Panel');
+  /**
+   * Create panel
+   *
+   * @param {Core} core Core reference
+   * @param {Object} options Options
+   */
+  constructor(core, options = {}) {
+    super('Panel');
 
-		this.core = core;
-		this.options = Object.assign(
-			{},
-			{
-				ontop: true,
-				position: 'top',
-				items: [],
-			},
-			options
-		);
+    this.core = core;
+    this.options = Object.assign(
+      {},
+      {
+        ontop: true,
+        position: 'top',
+        items: [],
+      },
+      options
+    );
 
-		this.dateString="";
-		this.date1="";
-		this.items = [];
-		this.inited = false;
-		this.destroyed = false;
-		this.$element = null;
+    this.dateString = '';
+    this.date1 = '';
+    this.items = [];
+    this.inited = false;
+    this.destroyed = false;
+    this.$element = null;
 
-		this.options.items.forEach(({ name, options }) => {
-			const c = core.make('osjs/panels').get(name);
-			this.addItem(new c(this.core, this, options || {}));
-		});
-		this.formatDate=this.formatDate.bind(this);
-	}
+    this.options.items.forEach(({name, options}) => {
+      const c = core.make('osjs/panels').get(name);
+      this.addItem(new c(this.core, this, options || {}));
+    });
+    this.formatDate = this.formatDate.bind(this);
+  }
 
-	/**
-	 * Destroys the panel
-	 */
-	destroy() {
-		if (this.destroyed) {
-			return;
-		}
+  /**
+   * Destroys the panel
+   */
+  destroy() {
+    if (this.destroyed) {
+      return;
+    }
 
-		this.items = this.items.filter(item => {
-			try {
-				item.destroy();
-			} catch (e) {
-				console.warn(e);
-			}
-			return false;
-		});
+    this.items = this.items.filter(item => {
+      try {
+        item.destroy();
+      } catch (e) {
+        console.warn(e);
+      }
+      return false;
+    });
 
-		this.destroyed = true;
-		this.inited = false;
-		this.emit('destroy');
-		this.core.emit('osjs/panel:destroy', this);
+    this.destroyed = true;
+    this.inited = false;
+    this.emit('destroy');
+    this.core.emit('osjs/panel:destroy', this);
 
-		this.$element.remove();
-		this.$element = null;
-	}
-
-
-	formatDate(string){
-	    var options = {  day: 'numeric', month: 'long' };
-    	return new Date(string).toLocaleDateString([],options);
-	}
-
-	/**
-	 * Initializes the panel
-	 */
-	init() {
-		if (this.inited) {
-			return;
-		}
-		this.destroyed = false;
-		this.inited = true;
-
-		const _ = this.core.make('osjs/locale').translate;
-		const __ = this.core.make('osjs/locale').translatable(languages);
-
-		this.$element = document.createElement('div');
-		this.$element.classList.add('osjs-panel');
-		// this.$element.classList.add('osjs__contextmenu');
-		this.$element.addEventListener('contextmenu', ev => {
-			ev.preventDefault();
-
-			const disabled = this.core.config('desktop.lock');
-			if (disabled) {
-				return;
-			}
-
-			this.core.make('osjs/contextmenu').show({
-				position: ev,
-				menu: [
-					{
-						label: __('LBL_PANEL_POSITION'),
-						items: [
-							{
-								label: _('LBL_TOP'),
-								onclick: () => this.setPosition('top'),
-							},
-							{
-								label: _('LBL_BOTTOM'),
-								onclick: () => this.setPosition('bottom'),
-							},
-						],
-					},
-				],
-			});
-		});
-		this.$element.setAttribute('data-position', this.options.position);
-		this.$element.setAttribute('data-ontop', String(this.options.ontop));
-		this.core.$root.appendChild(this.$element);
-		let appmenuElement = document.createElement('div');
-		appmenuElement.classList.add('appmenu');
-		appmenuElement.id = 'appmenu';
-		this.core.$root.appendChild(appmenuElement);
-		if (this.options.position === 'top') {
-			appmenuElement.style['margin-top'] = '3.5em';
-		} else {
-			appmenuElement.style['margin-top'] = '0';
-		}
+    this.$element.remove();
+    this.$element = null;
+  }
 
 
-		let profileElement = document.createElement('div');
-		profileElement.classList.add('profile');
-		let profileDetails = this.core.make('oxzion/profile').get();
-		// let profileInfo = { 
-		// 	icon: "gdhgh",
-		// 	firstname:"bharat",
-		// 	lastname:"g",
-		// 	email:"sfdg",
-		// 	address:"jkgj",
-		// 	date_of_birth:"",
-		// 	interest:""
-		// }
-		 let profileInfo = profileDetails['key'];
+  formatDate(string) {
+    let options = {day: 'numeric', month: 'long'};
+    return new Date(string).toLocaleDateString([], options);
+  }
 
-		this.dateString = profileInfo['date_of_birth'];
-		this.date1= this.formatDate(this.dateString);
+  /**
+   * Initializes the panel
+   */
+  init() {
+    if (this.inited) {
+      return;
+    }
+    this.destroyed = false;
+    this.inited = true;
 
-		let profileCard = document.createElement('div');
-		profileCard.innerHTML = 
-								'<br/> <a '
-										+'class="btn-floating btn-small red" '
-										+'onclick={OSjs.run("Preferences");document.getElementById("profile").classList.remove("profile-visible");}; '
-										+'id="editbutton">'
-										+'<i class="material-icons">edit</i>'
-										+'style="transition-delay:10s;"'
-										+'</a>'+
-									
-									'<img id="imgprofile" '
-										+'src='+profileInfo['icon']+" "
-										+'height="130" '
-										+'width="130" '
-										+'className="circle img-responsive"'
-										+'style="border-radius:50%;cursor:pointer;"'
-										+'onclick={OSjs.run("ImageUploader");document.getElementById("profile").classList.remove("profile-visible");}; />'+
+    const _ = this.core.make('osjs/locale').translate;
+    const __ = this.core.make('osjs/locale').translatable(languages);
 
-									'<label '
-										+'className="name" '
-										+'style="position: absolute;padding-top: 58px;padding-left: 25px;">'+profileInfo['firstname']+' '+profileInfo['lastname']+'</label>'+
-										
-									'<label '
-										+'className="name" '
-										+'style="position: absolute;padding-top: 82px;padding-left: 26px;">'+profileInfo['designation']+'</label> ';
-										
-		profileElement.appendChild(profileCard);
+    this.$element = document.createElement('div');
+    this.$element.classList.add('osjs-panel');
+    // this.$element.classList.add('osjs__contextmenu');
+    this.$element.addEventListener('contextmenu', ev => {
+      ev.preventDefault();
 
-		let emailElement = document.createElement('div');
-		emailElement.innerHTML = '<i class="fa-lg fa fa-envelope" id="iconi1" />'
-								+' <label id="info">'+profileInfo['email']+'</label> <br/>';
-		profileElement.appendChild(emailElement);
+      const disabled = this.core.config('desktop.lock');
+      if (disabled) {
+        return;
+      }
 
-
-		let phoneElement = document.createElement('div');
-		phoneElement.innerHTML = '<i class="fa-lg fa fa-phone" id="iconi" />'
-								+' <label id="info">'+profileInfo['phone']+'</label> <br/>';
-		profileElement.appendChild(phoneElement);
-
-
-		let addressElement = document.createElement('div');
-		addressElement.innerHTML = '<i class="fa-lg fa fa-location-arrow" id="iconi" />'
-								+' <label id="addresic">'+profileInfo['address']+'</label> <br/>';
-		profileElement.appendChild(addressElement);
-
-
-		let countryElement = document.createElement('div');
-		countryElement.innerHTML = '<i class="fa-lg fa fa-map-marker" id="iconi" />'
-								+' <label id="info">'+profileInfo['country']+'</label> <br/>';
-		profileElement.appendChild(countryElement);
+      this.core.make('osjs/contextmenu').show({
+        position: ev,
+        menu: [
+          {
+            label: __('LBL_PANEL_POSITION'),
+            items: [
+              {
+                label: _('LBL_TOP'),
+                onclick: () => this.setPosition('top'),
+              },
+              {
+                label: _('LBL_BOTTOM'),
+                onclick: () => this.setPosition('bottom'),
+              },
+            ],
+          },
+        ],
+      });
+    });
+    this.$element.setAttribute('data-position', this.options.position);
+    this.$element.setAttribute('data-ontop', String(this.options.ontop));
+    this.core.$root.appendChild(this.$element);
+    let appmenuElement = document.createElement('div');
+    appmenuElement.classList.add('appmenu');
+    appmenuElement.id = 'appmenu';
+    this.core.$root.appendChild(appmenuElement);
+    if (this.options.position === 'top') {
+      appmenuElement.style['margin-top'] = '3.5em';
+    } else {
+      appmenuElement.style['margin-top'] = '0';
+    }
 
 
-		let dobElement = document.createElement('div');
-		dobElement.innerHTML = '<i class="fa-lg fa fa-birthday-cake" id="iconi" />'
-								+' <label id="info">'+profileInfo['date_of_birth']+'</label> <br/>';
-		profileElement.appendChild(dobElement);
+    let profileElement = document.createElement('div');
+    profileElement.classList.add('profile');
+    let profileDetails = this.core.make('oxzion/profile').get();
+    let profileInfo = profileDetails['key'];
+    this.dateString = profileInfo['date_of_birth'];
+    this.date1 = this.formatDate(this.dateString);
+
+    let profileCard = document.createElement('div');
+    profileCard.innerHTML = '<br/> <a '
+                     + 'class="btn-floating btn-small red" '
+                     + 'onclick={OSjs.run("Preferences");document.getElementById("profile").classList.remove("profile-visible");}; '
+                     + 'id="editbutton">'
+                     + '<i class="material-icons">edit</i>'
+                     + 'style="transition-delay:10s;"'
+                     + '</a>' + '<img id="imgprofile" '
+                     + 'src=' + profileInfo['icon'] + ' '
+                     + 'height="130" '
+                     + 'width="130" '
+                     + 'className="circle img-responsive"'
+                     + 'style="border-radius:50%;cursor:pointer;"'
+                     + 'onclick={OSjs.run("ImageUploader");document.getElementById("profile").classList.remove("profile-visible");}; />' + '<label '
+                     + 'className="name" '
+                     + 'style="position: absolute;padding-top: 58px;padding-left: 25px;">' + profileInfo['firstname'] + ' ' + profileInfo['lastname'] + '</label>' + '<label '
+                    + 'className="name" '
+                    + 'style="position: absolute;padding-top: 82px;padding-left: 26px;">' + profileInfo['designation'] + '</label>';
+    profileElement.appendChild(profileCard);
+    let emailElement = document.createElement('div');
+    emailElement.innerHTML = '<i class="fa-lg fa fa-envelope" id="iconi1" />'
+                + ' label id="info">' + profileInfo['email'] + '</lbel> <br/>';
+    profileElement.appendChild(emailElement);
 
 
-		let interestElement = document.createElement('div');
-		interestElement.innerHTML = '<i class="fa-lg fa fa-heart" id="iconi" />'
-								+' <label id="info">'+profileInfo['interest']+'</label> <br/>';
-		profileElement.appendChild(interestElement);
-		
-		
-		
-		profileElement.id = 'profile';
-		this.core.$root.appendChild(profileElement);
-		
-		if (this.options.position === 'top') {
-			profileElement.style['margin-top'] = '3.5em';
-		} else {
-			profileElement.style['margin-top'] = '0';
-		}
-		this.items.forEach(item => item.init());
-		this.emit('create');
-	}
+    let phoneElement = document.createElement('div');
+    phoneElement.innerHTML = '<i class="fa-lg fa fa-phone" id="iconi" />'
+                + ' label id="info">' + profileInfo['phone'] + '</lbel> <br/>';
+    profileElement.appendChild(phoneElement);
 
-	/**
-	 * Add an item to the panel
-	 * @param {PanelItem} item The panel item instance
-	 */
-	addItem(item) {
-		if (!(item instanceof PanelItem)) {
-			throw new TypeError('Invalid panel item specified');
-		}
 
-		this.items.push(item);
+    let addressElement = document.createElement('div');
+    addressElement.innerHTML = '<i class="fa-lg fa fa-location-arrow" id="iconi" />'
+                + ' label id="addresic">' + profileInfo['address'] + '</lbel> <br/>';
+    profileElement.appendChild(addressElement);
+    let countryElement = document.createElement('div');
+    countryElement.innerHTML = '<i class="fa-lg fa fa-map-marker" id="iconi" />'
+                + ' label id="info">' + profileInfo['country'] + '</lbel> <br/>';
+    profileElement.appendChild(countryElement);
+    let dobElement = document.createElement('div');
+    dobElement.innerHTML = '<i class="fa-lg fa fa-birthday-cake" id="iconi" />'
+                + ' label id="info">' + profileInfo['date_of_birth'] + '</lbel> <br/>';
+    profileElement.appendChild(dobElement);
 
-		if (this.inited) {
-			item.init();
-		}
-	}
 
-	setPosition(position) {
-		this.options.position = position;
-		let appMenu = document.getElementById('appmenu');
-		if (position === 'top') {
-			appMenu.style['margin-top'] = '3.5em';
-		} else {
-			appMenu.style['margin-top'] = '0';
-		}
-		return this.core
-			.make('osjs/panels')
-			.save()
-			.then(() => {
-				const desktop = this.core.make('osjs/desktop');
-				return desktop.applySettings();
-			});
-	}
+    let interestElement = document.createElement('div');
+    interestElement.innerHTML = '<i class="fa-lg fa fa-heart" id="iconi" />'
+                + ' label id="info">' + profileInfo['interest'] + '</lbel> <br/>';
+    profileElement.appendChild(interestElement);
+    profileElement.id = 'profile';
+    this.core.$root.appendChild(profileElement);
+    if (this.options.position === 'top') {
+      profileElement.style['margin-top'] = '3.5em';
+    } else {
+      profileElement.style['margin-top'] = '0';
+    }
+    this.items.forEach(item => item.init());
+    this.emit('create');
+  }
+
+  /**
+   * Add an item to the panel
+   * @param {PanelItem} item The panel item instance
+   */
+  addItem(item) {
+    if (!(item instanceof PanelItem)) {
+      throw new TypeError('Invalid panel item specified');
+    }
+
+    this.items.push(item);
+
+    if (this.inited) {
+      item.init();
+    }
+  }
+
+  setPosition(position) {
+    this.options.position = position;
+    let appMenu = document.getElementById('appmenu');
+    if (position === 'top') {
+      appMenu.style['margin-top'] = '3.5em';
+    } else {
+      appMenu.style['margin-top'] = '0';
+    }
+    return this.core
+      .make('osjs/panels')
+      .save()
+      .then(() => {
+        const desktop = this.core.make('osjs/desktop');
+        return desktop.applySettings();
+      });
+  }
 }
