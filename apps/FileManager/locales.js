f8b6097c03cfae4a2b5607d80c2db7623aa08b46
bharatg <bharatg@myvamla.com>
2019-03-25 14:46:33 +0530

    Add File Manager Application

---
+++ apps/FileManager/locales.js
@@ -0,0 +1,14 @@
+export const en_EN = {
+  LBL_SHOW_HIDDEN_FILES: 'Show hidden files',
+  LBL_MINIMALISTIC: 'Minimalistic'
+};
+
+export const nb_NO = {
+  LBL_SHOW_HIDDEN_FILES: 'Vis skjulte filer',
+  LBL_MINIMALISTIC: 'Minimalistisk'
+};
+
+export const vi_VN = {
+  LBL_SHOW_HIDDEN_FILES: 'Hiện các tập tin ẩn',
+  LBL_MINIMALISTIC: 'Tối giản'
+};
