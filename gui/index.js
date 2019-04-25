f691b6c35837eecb154669e408885ffe53b2e43e
Sadhitha <saditha@myvamla.com>
2019-04-25 19:34:01 +0530

    Shell for GUI

---
+++ gui/index.js
@@ -0,0 +1,7 @@
+import LeftMenuTemplate from "./src/leftmenutemplate";
+import RenderTemplate from "./src/rendertemplate";
+
+export {
+    LeftMenuTemplate,
+    RenderTemplate
+}
---
+++ gui/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const minimize = mode === 'production';
+const plugins = [];
+
+if (mode === 'production') {
+  plugins.push(new OptimizeCSSAssetsPlugin({
+    cssProcessorOptions: {
+      discardComments: true
+    },
+  }));
+}
+
+module.exports = {
+  mode,
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'index.js')
+  ],
+  optimization: {
+    minimize,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    ...plugins
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(svg|png|jpe?g|gif|webp)$/,
+        use: [{
+          loader: "file-loader"
+        }]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        include: /typeface/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[ext]"
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              minimize,
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
