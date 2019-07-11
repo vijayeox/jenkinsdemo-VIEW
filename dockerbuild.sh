8aa8a7b86de4202d6c31674d6779a917d675479c
danishsultan <danishsultan007@gmail.com>
2019-07-11 19:53:03 +0530

    updated the build for executing specifically

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -4,5 +4,9 @@ cd bos
 npm rebuild node-sass
 cd ..
 ./clean.sh
-./build.sh
+./build.sh bos
+./build.sh gui
+./build.sh iconpacks
+./build.sh themes
+./build.sh apps "Admin, Announcements, Calculator, Calendar, Chat, Contacts, CRM, ImageUploader, Mail, MailAdmin, Preferences"
 echo build completed successfully
