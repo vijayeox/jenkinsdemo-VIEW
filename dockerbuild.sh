7db3e3a065f46339c91dc04f27dcc58334a788aa
danishsultan <danishsultan007@gmail.com>
2019-07-11 21:29:18 +0530

    building bos in last to avoid missing package:discover commented openproject in full deployment

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -4,9 +4,9 @@ cd bos
 npm rebuild node-sass
 cd ..
 ./clean.sh
-./build.sh bos
 ./build.sh gui
 ./build.sh iconpacks
 ./build.sh themes
 ./build.sh apps "Admin, Announcements, Calculator, Calendar, Chat, Contacts, CRM, ImageUploader, Mail, MailAdmin, Preferences"
+./build.sh bos
 echo build completed successfully
