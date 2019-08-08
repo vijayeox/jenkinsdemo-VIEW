f3b087826a921d818e83ecf75c0d0f2e97826dc5
purvi <purvi@myvamla.com>
2019-08-08 11:24:46 +0530

    Added a json file for ease of addition of apps

--- apps/HelpApp/helpPage.js
+++ apps/HelpApp/helpPage.js
@@ -1,24 +1,23 @@
 import './index.scss';
 import React, { Component } from 'react';
-import { DropdownButton, Dropdown, Nav, NavDropdown } from 'react-bootstrap';
-import ReactDOM from 'react-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
+import { Nav } from 'react-bootstrap';
+//import ReactDOM from 'react-dom';
+import { myObj } from './topic.json';
 
 const baseUrl = process.env.SERVER;
+
 class HelpPage extends Component {
   constructor(props) {
     super(props);
     this.menuClick = this.menuClick.bind(this);
     this.appClick = this.appClick.bind(this);
-    this.state = { topic: null};
+    this.state = { topic: null };
   }
 
   menuClick() {
     console.log("In menuclick");
     var x = document.getElementById("myLinks");
-    if(!x){
+    if (!x) {
       return;
     }
     if (x.style.display === "block") {
@@ -27,63 +26,53 @@ class HelpPage extends Component {
       x.style.display = "block";
     }
   }
+
   appClick(e) {
-    // Create an iframe
     console.log("inside handleClick");
-    console.log(document.getElementById("crm").href);
-    // const iframe = document.createElement('iframe');
-    // iframe.style.width = '100%';
-    // iframe.style.height = '100%';
-    // console.log(proc.resource(baseUrl));
-    const topic =e.target.id;
-    console.log(baseUrl);
-    this.setState({ topic});
-    this.menuClick();
+    console.log(e.target.id);
+    this.setState({ topic: e.target.id });
   }
 
   render() {
-    console.log(this.state);
-    if(this.state.topic){
-      const styles = {width:'100%', height:'100%',border:0};
-      return (   
+    if (this.state.topic) {
+      const styles = { width: '100%', height: '100%', border: 0 };
+      return (
         <div style={styles}>
-          <Nav className="justify-content-end" activeKey="/home" >      
-            
-              <div className="topnav active" >
-                <div className="font-2_5em home">
-                  <i className="fa fa-home" onClick={this.appClick} ></i>
-                </div>
-                <div className="links" id="myLinks">
-                  <a href="#" onClick={this.appClick} id="crm">CRM</a>
-                  <a href="#" onClick={this.appClick} >Task</a>
-                </div>
-                <a href="javascript:void(0);" className="icon" onClick={this.menuClick}>
-                  <i className="fa fa-ellipsis-v"></i></a>
+          <Nav className="justify-content-end" activeKey="/home" >
+            <div className="topnav active" >
+              <div className="font-2_5em home">
+                <i className="fa fa-home" onClick={this.appClick}></i>
               </div>
-            
+              <div className="links" id="myLinks">
+                {myObj.map((CurrentValue, index) => {
+                  return (
+                    <a key={index} onClick={this.appClick} id={CurrentValue.id}>
+                      {CurrentValue.name}</a>
+                  )
+                })}
+              </div>
+              <a href="javascript:void(0);" className="icon" onClick={this.menuClick}>
+                <i className="fa fa-ellipsis-v"></i></a>
+            </div>
           </Nav>
           <iframe style={styles} src={this.props.args.proc.resource(baseUrl + this.state.topic)} ></iframe>
         </div>
       );
-    }else{
+    } else {
       return (
-      <div className="container">
-        <h3 className="topic-title">Help Topics</h3>
-        <a href="#"><h4 onClick={this.appClick} id="crm">CRM</h4></a>
-        <a href="#"><h4 onClick={this.appClick} >Task</h4></a>
-      </div>
-      );
-    }
-      // <Nav className="justify-content-end" activeKey="/home" >        
-      //   <DropdownButton id="dropdown-basic-button" drop="left" iconClass='fa fa-ellipsis-v'>
-      //   <Dropdown.Item as="button" onClick={this.handleClick} eventKey="crm">CRM</Dropdown.Item>
-      //   <Dropdown.Item as="button" eventKey="task">Task</Dropdown.Item>
-      // </DropdownButton>
-      // </Nav>
-    ;
+        <div className="container">
+          <h3 className="topic-title">Help Topics</h3>
+          {myObj.map((CurrentValue, index) => {
+            return (
+              <div className="help-menu" key={index}  >
+                <h4 onClick={this.appClick} id={CurrentValue.id}>{CurrentValue.name}</h4>
+              </div>
+            )
+          })}
+        </div>
+      )
+    };
   }
 }
-
-//onClick={this.handleClick}
 
 export default HelpPage;
