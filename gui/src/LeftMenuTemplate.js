d50f2d0701f9266ab6120d7f891f01421f48b032
Prajwa K <prajwal.k1@gmail.com>
2019-09-04 15:23:30 +0530

    Close left menu on item selected. Make left menu to controlled component type

--- gui/src/LeftMenuTemplate.js
+++ gui/src/LeftMenuTemplate.js
@@ -1,10 +1,16 @@
 import React from "react";
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-import { Button, ButtonGroup } from '@trendmicro/react-buttons';
-import Dropdown, { MenuItem } from '@trendmicro/react-dropdown';
-import Page from './components/App/Page';
+import SideNav, {
+  Toggle,
+  Nav,
+  NavItem,
+  NavIcon,
+  NavText
+} from "@trendmicro/react-sidenav";
+import { Button, ButtonGroup } from "@trendmicro/react-buttons";
+import Dropdown, { MenuItem } from "@trendmicro/react-dropdown";
+import Page from "./components/App/Page";
 // Be sure to include styles at some point, probably during your bootstraping
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
 class LeftMenuTemplate extends React.Component {
   constructor(props) {
@@ -13,71 +19,92 @@ class LeftMenuTemplate extends React.Component {
     this.appId = this.props.appId;
     this.state = {
       menus: [],
-      selected: '',
+      selected: "",
       expanded: false
-    }
+    };
 
     this.getMenulist().then(response => {
       this.setState({
-        menus : response["data"]
-      })
-    })
+        menus: response["data"]
+      });
+    });
 
-    this.onSelect=this.onSelect.bind(this);
-    this.onToggle=this.onToggle.bind(this);
-    this.navigate=this.navigate.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+    this.onToggle = this.onToggle.bind(this);
   }
-// REMOVE THE HARD CODED APP ID
-async getMenulist() {
-  let helper = this.core.make('oxzion/restClient');
-  let menulist = await helper.request('v1','/app/'+this.appId+'/menu', {}, 'get' );
-  return menulist;
-};
-onToggle(expanded){
-  this.setState({ expanded: expanded });
-}
-onSelect(selected){
-  this.setState({ selected: selected });
-}
-navigate(pathname) {
-  this.setState({ selected: pathname });
-}
+  // REMOVE THE HARD CODED APP ID
+  async getMenulist() {
+    let helper = this.core.make("oxzion/restClient");
+    let menulist = await helper.request(
+      "v1",
+      "/app/" + this.appId + "/menu",
+      {},
+      "get"
+    );
+    return menulist;
+  }
+  onToggle(expanded) {
+    this.setState({ expanded: expanded });
+  }
+  onSelect(selected) {
+    this.setState({ selected: selected, expanded: false });
+  }
 
-render() {
-  const { expanded ,selected} = this.state;
-  let selection;
-  if (this.state.selected.page_id) {
-    selection = <Page pageId={this.state.selected.page_id} config={this.props.config} app={this.props.appId} core={this.core}/>
+  render() {
+    const { expanded, selected } = this.state;
+    let selection;
+    if (this.state.selected.page_id) {
+      selection = (
+        <Page
+          pageId={this.state.selected.page_id}
+          config={this.props.config}
+          app={this.props.appId}
+          core={this.core}
+        />
+      );
+    }
+    return (
+      <div
+        style={{
+          height: "100%",
+          overflow: "auto"
+        }}
+      >
+        <div
+          style={{
+            marginLeft: expanded ? 240 : 64,
+            padding: "15px 20px 0 20px"
+          }}
+        >
+          {selection}
+        </div>
+        <SideNav
+          onSelect={this.onSelect}
+          onToggle={this.onToggle}
+          expanded={this.state.expanded}
+        >
+          <SideNav.Toggle />
+          <SideNav.Nav selected={selected}>
+            {this.state.menus.map((menuitem, index) => {
+              return (
+                <NavItem eventKey={menuitem} key={index}>
+                  <NavIcon>
+                    <i
+                      className={menuitem.icon}
+                      name={menuitem.name}
+                      style={{ fontSize: "1.5em", verticalAlign: "middle" }}
+                    />
+                  </NavIcon>
+                  <NavText style={{ paddingRight: 32 }} name={menuitem.name}>
+                    {menuitem.name}
+                  </NavText>
+                </NavItem>
+              );
+            })}
+          </SideNav.Nav>
+        </SideNav>
+      </div>
+    );
   }
-  return (
-    <div
-    style={{
-      height: '100%',
-      overflow: 'auto'
-    }}>
-    <div
-    style={{
-      marginLeft: expanded ? 240 : 64,
-      padding: '15px 20px 0 20px'
-    }} >   
-    {selection}
-    </div>
-    <SideNav onSelect={this.onSelect} onToggle={this.onToggle}>
-    <SideNav.Toggle />
-    <SideNav.Nav selected={selected}>
-    { this.state.menus.map((menuitem, index) => {
-     return  <NavItem eventKey={menuitem} key={index}>
-     <NavIcon>
-     <i className={menuitem.icon} name={menuitem.name} style={{ fontSize: '1.5em', verticalAlign: 'middle' }} />
-     </NavIcon>
-     <NavText style={{ paddingRight: 32 }} name={menuitem.name}>
-     {menuitem.name}
-     </NavText>
-     </NavItem>
-   })}
-   </SideNav.Nav>
-   </SideNav>
-   </div>
-   );
-  }}
-  export default LeftMenuTemplate;
+}
+export default LeftMenuTemplate;
