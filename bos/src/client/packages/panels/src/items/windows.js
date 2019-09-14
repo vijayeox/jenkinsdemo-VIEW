2679fa110a09bf3cc635865f409a871e7a72c921
bharatg <bharatg@myvamla.com>
2019-09-14 17:57:11 +0530

    Add Close Button Capability for Windows in Tray Menu

--- bos/src/client/packages/panels/src/items/windows.js
+++ bos/src/client/packages/panels/src/items/windows.js
@@ -30,6 +30,7 @@
 
 import {h} from 'hyperapp';
 import PanelItem from '../panel-item';
+const closeIcon = require('../close.png');
 
 const mapWindow = win => {
   return {
@@ -179,6 +180,14 @@ export default class WindowsPanelItem extends PanelItem {
         alt: w.title || '(window)',
       }),
       h('span', {}, w.title || '(window)'),
+      h('div', {
+        className: 'window-tray-close-button',
+      },
+      [h('img', {
+        src: closeIcon,
+        alt: 'close',
+        onclick: () => w.close(),
+      })]),
     ]));
 
     const special = state.launchers.map(name => h('div', {
