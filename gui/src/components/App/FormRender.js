63af1a2b6b9955d216faf90de67b14ca97d6b397
bharatg <bharatg@myvamla.com>
2019-09-16 19:56:39 +0530

    Form Delegate capability

--- gui/src/components/App/FormRender.js
+++ gui/src/components/App/FormRender.js
@@ -1,6 +1,8 @@
 import "../../public/css/formstyles.scss";
 import {Formio} from 'formiojs';
+import {getComponent,flattenComponents} from 'formiojs/utils/formUtils'
 import React from 'react';
+// import ConvergePayCheckoutComponent from './Form/Payment/ConvergePayCheckoutComponent';
 import 'bootstrap';
 
 class FormRender extends React.Component  {
@@ -22,6 +24,11 @@ class FormRender extends React.Component  {
     }
     this.formDivID = "formio_"+this.state.formId;
   }
+  async callDelegate(delegate,params) {
+    let helper = this.core.make('oxzion/restClient');
+    let delegateData = await helper.request('v1','/app/'+this.state.appId+'/delegate/'+delegate, params, 'post' );
+    return delegateData;
+  }
   async getWorkflow() {
     // call to api using wrapper
     let helper = this.core.make('oxzion/restClient');
@@ -33,12 +40,6 @@ class FormRender extends React.Component  {
     let helper = this.core.make('oxzion/restClient');
     let formContent = await helper.request('v1','/activity/'+this.state.activityInstanceId+'/form', {}, 'get' );
     return formContent;
-  }
-  async getInstanceData() {
-    // call to api using wrapper
-    // let helper = this.core.make('oxzion/restClient');
-    // let pageContent = await helper.request('v1','/app/'+this.state.appId+'/form/' + this.state.formId+'/workflow', {}, 'get' );
-    // return pageContent;
   }
   async saveForm(data) {
     let helper = this.core.make('oxzion/restClient');
@@ -103,7 +104,8 @@ class FormRender extends React.Component  {
   createForm(){
     let that =this
     if(this.state.content && !this.state.form){
-      var formCreated = Formio.createForm(document.getElementById(this.formDivID), this.state.content).then(function(form){
+      // Formio.registerComponent('convergepay', ConvergePayCheckoutComponent);
+      var formCreated = Formio.createForm(document.getElementById(this.formDivID), JSON.parse(this.state.content),{}).then(function(form){
        form.on('render', function(){
         if(that.state.data){
           form.setSubmission(that.state.data);
@@ -111,18 +113,84 @@ class FormRender extends React.Component  {
         if(that.state.page){
           form.setPage(that.state.page);
         }
-       });
+      });
        form.on('prevPage',function(prevPage,submission){
         that.setState({page:prevPage});
-       });
+      });
        form.on('nextPage',function(nextPage,submission){
         that.setState({page:nextPage});
-       });
+      });
        form.on('submit', function(submission) {
         that.saveForm(submission.data);
       });
        form.on('change', function(changed) {
         console.log('Form was changed', changed);
+        var formdata = changed;
+        var formdataArray = [];
+        for (var formDataItem in formdata.data) {
+          if (formdata.data.hasOwnProperty(formDataItem)) {
+            formdataArray[formDataItem] = formdata.data[formDataItem];
+          }
+        }
+        if(changed && changed.changed){
+          var component = changed.changed.component;
+          var properties = component.properties;
+          if(properties){
+            if(properties['delegate']){
+              that.callDelegate(properties['delegate'],formdata.data).then(response => {
+                var responseArray = [];
+                for (var responseDataItem in response.data) {
+                  if (response.data.hasOwnProperty(responseDataItem)) {
+                    responseArray[responseDataItem] = response.data[responseDataItem];
+                  }
+                }
+                if(response.data){
+                  form.submission = { data: response.data };
+                  form.triggerChange();
+                }
+              });
+            }
+            if(properties['target']){
+              var targetComponent = form.getComponent(properties['target']);
+              if(changed.changed.value){
+                var value = formdata.data[changed.changed.value];
+                if(value){
+                  targetComponent.setValue(value);
+                } else {
+                  targetComponent.setValue(changed.changed.value);
+                }
+              }
+            }
+          }
+        }
+        var componentList = flattenComponents(form.components);
+        for (var componentKey in componentList) {
+         if (componentList.hasOwnProperty(componentKey)) {
+          var componentItem = componentList[componentKey];
+          if(componentItem.component.properties && componentItem.component.properties['target']){
+            var targetComponent = form.getComponent(componentItem.component.properties['target']);
+            if(targetComponent && componentItem.value){
+              if(formdataArray[componentItem.value]){
+                targetComponent.setValue(formdataArray[componentItem.value]);
+                targetComponent.refresh();
+              }
+            }
+          }
+        }
+      }
+    });
+       form.on('callDelegate', function(data) {
+        var component = form.getComponent(event.target.id);
+        var properties = component.component.properties;
+        if(properties){
+          if(properties['delegate']){
+            that.callDelegate(properties['delegate'],data).then(response => {
+              if(response.data){
+                form.submission = { data: response.data };
+              }
+            });
+          }
+        }
       });
      });
     }
@@ -131,20 +199,20 @@ class FormRender extends React.Component  {
     this.loadWorkflow();
   }
 
-async PushDataPOST(api, method, item, body) {
-  if (method == "put") {
-    let response = await helper.request(
-      "v1",
-      "/" + api + "/" + item,
-      body,
-      "filepost"
-    );
-    return response;
-  } else if (method == "post") {
-    let response = await helper.request("v1", "/" + api, body, "filepost");
-    return response;
+  async PushDataPOST(api, method, item, body) {
+    if (method == "put") {
+      let response = await helper.request(
+        "v1",
+        "/" + api + "/" + item,
+        body,
+        "filepost"
+        );
+      return response;
+    } else if (method == "post") {
+      let response = await helper.request("v1", "/" + api, body, "filepost");
+      return response;
+    }
   }
-}
   render(){
     return (<div className="form-render" id={this.formDivID}></div>);
   }
