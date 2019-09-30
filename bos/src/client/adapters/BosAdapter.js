c1a7ee4bb5b82a20ba277d14fad3c81610da0ffc
purvi <purvi@myvamla.com>
2019-09-30 19:14:02 +0530

    missing bracket

--- bos/src/client/adapters/BosAdapter.js
+++ bos/src/client/adapters/BosAdapter.js
@@ -3,50 +3,47 @@ import LocalStorageAdapter from './localStorageAdapter.js';
 
 export class BosAdapter extends ServiceProvider {
 
-	constructor(core, options = {}) {
-		super(core, options || {});
-		this.core = core;
+    constructor(core, options = {}) {
+        super(core, options || {});
+        this.core = core;
         this.metadata = [];
-	}
+    }
 
 
-	providers() {
-		return [
-			'oxzion/core'
-		];
-	}
+    providers() {
+      return [
+        'oxzion/core'
+      ];
+    }
 
-	async init() {
-		this.core.on('osjs/core:started', () => {
-            document.body.classList.add('osjs-root');
-            var queryString = window.location.search.substr(1);
-            console.log(queryString);   
-            if (queryString) {
-                var queryObj = queryString.split("&").reduce(function(prev, curr, i, arr) {
-                    var p = curr.split("=");
-                    prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1]);
-                    return prev;
-                }, {});
-                
-				var appName = queryObj.app;
+  async init() {
+        this.core.on('osjs/core:started', () => {
+        document.body.classList.add('osjs-root');
+        var queryString = window.location.search.substr(1);
+        console.log(queryString);
+        if (queryString) {
+            var queryObj = queryString.split("&").reduce(function(prev, curr, i, arr) {
+                var p = curr.split("=");
+                prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1]);
+                return prev;
+            }, {});
+            var appName = queryObj.app;
+            var userDetails = this.core.make('oxzion/profile').get();
+            var appList = userDetails.key.blackListedApps;
 
-            	var userDetails = this.core.make('oxzion/profile').get();
-            	var appList = userDetails.key.blackListedApps;
+            if (!(appName in userDetails.key.blackListedApps)){
 
-            	if (!(appName in userDetails.key.blackListedApps)){
-
-	                this.core.request(this.core.config('packages.manifest'), {}, 'json')
-	                .then(metadata => {
-	                    this.addPackages(metadata);
-	                    this.launch(queryObj);
-	                })
-	                .catch(error => console.error(error));
-	            }
-            }
-        }
+               this.core.request(this.core.config('packages.manifest'), {}, 'json')
+               .then(metadata => {
+                   this.addPackages(metadata);
+                   this.launch(queryObj);
+               })
+               .catch(error => console.error(error));
+           }
+       }
     });
 
-      this.core.on('osjs/core:started', () => {
+        this.core.on('osjs/core:started', () => {
         var myDate = new Date();
         var hrs = myDate.getHours();
         var greet;
@@ -58,28 +55,29 @@ export class BosAdapter extends ServiceProvider {
             greet = 'Good Evening';
             //console.log(this.core.make('oxzion/profile').get().UserInfo();
             var userDetails = this.core.make('oxzion/profile').get();
-		this.core.make('osjs/notification', {  timeout:10000,icon:userDetails['key']['icon'],title:greet,message: 'Hello '+userDetails['key']['firstname']+'!'})});
-      this.core.on('oxzion/application:launch', (params) =>{
-        if(params.app){
-            this.core.make('osjs/packages').launch(params.app, ((params.args) ? params.args : {}), ((params.options) ? params.options : {}));
-        }
-    });
-  }
+            this.core.make('osjs/notification', {  timeout:10000,icon:userDetails['key']['icon'],title:"Welcome to 3.0!",message: 'Hello and '+greet+" "+userDetails['key']['firstname']+'!'})
+        });
 
-  addPackages(list) {
-    if (list instanceof Array) {
-        const append = list
-        .map(iter => Object.assign({
-            type: 'application'
-        }, iter));
-        this.metadata = [...this.metadata, ...append];
+        this.core.on('oxzion/application:launch', (params) =>{
+            if(params.app){
+                this.core.make('osjs/packages').launch(params.app, ((params.args) ? params.args : {}), ((params.options) ? params.options : {}));
+            }
+        });
     }
-}
 
-launch(app) {
-    var found = this.metadata.find(pkg => pkg.name === app.app);
-    if (found != undefined)
-        this.core.make('osjs/packages').launch(app.app, ((app.args) ? app.args : {}), ((app.options) ? app.options : {}));
-}
+    addPackages(list) {
+        if (list instanceof Array) {
+            const append = list
+            .map(iter => Object.assign({
+                type: 'application'
+            }, iter));
+            this.metadata = [...this.metadata, ...append];
+        }
+    }
 
+    launch(app) {
+        var found = this.metadata.find(pkg => pkg.name === app.app);
+        if (found != undefined)
+            this.core.make('osjs/packages').launch(app.app, ((app.args) ? app.args : {}), ((app.options) ? app.options : {}));
+    }
 }
