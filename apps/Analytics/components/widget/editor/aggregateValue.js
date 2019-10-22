32c3aef3a0d4916d32e914801cc50e9dfe1c4d33
ravi <ravi@firespike.in>
2019-10-22 07:06:37 +0530

    Completed pie chart and aggregate value widgets.

--- apps/Analytics/components/widget/editor/aggregateValue.js
+++ apps/Analytics/components/widget/editor/aggregateValue.js
@@ -1,39 +1,211 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Overlay, Tooltip} from 'react-bootstrap';
 
 class AggregateValue extends React.Component {
     constructor(props) {
         super(props);
         this.state = props.widget;
+        this.state.errors = {};
+        this.state.dataSetColumns = [];
+    }
+
+    graphConfigurationToState = () => {
+    }
+    
+    stateToGraphConfiguration = () => {
+    }
+
+    isUserInputValid = (field, value) => {
+        return true;
+    }
+
+    setWidget = (widget) => {
+        if (!widget) {
+            return;
+        }
+        this.setState((state) => {
+            for (let[key, value] of Object.entries(widget)) {
+                state[key] = value;
+            }
+            return state;
+        },
+        () => {
+            this.graphConfigurationToState();
+        });
+    }
+
+    setWidgetData = (data) => {
+        this.state.data = data;
+        this.detectDataSetColumns(data);
+        this.draw();            
+    }
+
+    detectDataSetColumns = (dataSet) => {
+        if (!dataSet) {
+            this.setState((state) => {
+                state.dataSetColumns = [];
+                return state;
+            });
+            return;
+        }
+        let columnsMap = {};
+        if (dataSet && dataSet.forEach) { //dataSet.forEach checks whether dataSet is an array.
+            dataSet.forEach(function(item, index, array) {
+                if (item) {
+                    for (let [column, value] of Object.entries(item)) {
+                        columnsMap[column] = true;
+                    }
+                }
+            });
+        }
+        let options = [];
+        for (let[column, flag] of Object.entries(columnsMap)) {
+            options.push({
+                value:column, 
+                label:column
+            });
+        }
+        options.sort(function(a, b) {
+            if (a.label < b.label) {
+                return -1;
+            }
+            if (a.label > b.label) {
+                return 1;
+            }
+            return 0;
+        });
+        this.setState((state) => {
+            state.dataSetColumns = options;
+            return state;
+        });
+    }
+
+    draw = () => {
+        let previewElement = document.querySelector('span#previewElement');
+        let type = typeof this.state.data;
+        if ((type === 'number') || (type === 'string')) {
+            previewElement.innerHTML = this.state.data;
+        }
+        else {
+            previewElement.innerHTML = '[Invalid data. Please select aggregate value query.]';
+        }
+    }
+
+    makeReadOnly = (flag) => {
+        this.setState({
+            readOnly : flag
+        });
+    }
+
+    //Called by widgetEditorApp.hasUserInputErrors
+    hasUserInputErrors = () => {
+        if (this.areAllFieldsValid()) {
+            return false;
+        }
+        var errors = this.state.errors;
+        var errorsFound = false;
+        for (let [key, value] of Object.entries(errors)) {
+            if (value) {
+                errorsFound = true;
+                break;
+            }
+        }
+        return errorsFound;
+    }
+
+    areAllFieldsValid = () => {
+        if (!this.refs) {
+            console.debug('No field with "ref" attribute. Validation success.');
+            return true;
+        }
+        let valid = true;
+        for (let [key, value] of Object.entries(this.refs)) {
+            if ((value.tagName === 'INPUT') || 
+                (value.tagName === 'SELECT')) {
+                valid = valid & this.isUserInputValid(value.name, value.value);
+            }
+            else {
+                console.debug(`Did not validate ref "${key}" having tag name "${value.tagName}"`);
+            }
+        }
+        console.debug('Validation of all widget fields result:' + valid);
+        return valid;
+    }
+
+    setErrorMessage = (key, message) => {
+        this.setState((state) => {
+            if (!message || ('' === message)) {
+                delete state.errors[key];
+            }
+            else {
+                state.errors[key] = message;
+            }
+            return state;
+        });
     }
 
     componentDidMount() {
-        window.postDataRequest(`analytics/widget/${this.state.uuid}?data=true`).
-            then(function(responseData) {
-                var previewElement = document.querySelector('span#widgetPreview');
-                previewElement.innerHTML = responseData.widget.data;
-            });
     }
 
     render() {
+        let thiz = this;
+        function dataSetColumns() {
+            let options = [];
+            thiz.state.dataSetColumns.map((item, key) => {
+                options.push(
+                    <option value={item.value}>{item.label}</option>
+                );
+            });
+            return options;
+        }
+
         return (
             <>
                 <div className="form-group col">
-                    <label>Value properties</label>
-                    <div className="form-control">
-                        <div className="form-row">
+                    <div className="card" id="chartPropertyBox">
+                        <div className="card-header">
+                            Aggregate value properties
+                        </div>
+                        <div className="card-body">
+                            <div className="form-group row">
+                                <label htmlFor="dataColumn" className="col-4 col-form-label form-control-sm">Data column</label>
+                                <div className="col-8">
+                                    <select id="dataColumn" name="dataColumn" ref="dataColumn" className="form-control form-control-sm" 
+                                        onChange={this.selectionChanged} disabled={this.state.readOnly} 
+                                        value={this.state.dataColumn ? this.state.dataColumn : ''}>
+                                        <option value="">-Select data column-</option>
+                                        { dataSetColumns() }
+                                    </select>
+                                    <Overlay target={this.refs.dataColumn} show={this.state.errors.dataColumn != null} placement="left">
+                                        {props => (
+                                        <Tooltip id="dataColumn-tooltip" {...props} className="error-tooltip">
+                                            {this.state.errors.dataColumn}
+                                        </Tooltip>
+                                        )}
+                                    </Overlay>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="form-group col">
-                    <label>Preview</label>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td align="center" valign="middle"><span id="widgetPreview">[Loading...]</span></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div className="card" id="chartPreviewBox">
+                        <div className="card-header">
+                            Preview
+                        </div>
+                        <div className="card-body">
+                            <div id="chartPreview">
+                                <table style={{border:'1px solid black', width:'100%', fontSize:'2em'}}>
+                                    <tbody>
+                                        <tr>
+                                            <td align="center" valign="middle"><span id="previewElement">[Loading...]</span></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
         );
--- apps/Analytics/components/widget/editor/pieChart.js
+++ apps/Analytics/components/widget/editor/pieChart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Overlay, Tooltip} from 'react-bootstrap';
 import BaseChart from './baseChart';
 
 class PieChart extends BaseChart {
@@ -7,11 +8,81 @@ class PieChart extends BaseChart {
         super(props);
     }
 
+    graphConfigurationToState = () => {
+        this.setState((state) => {
+            let configuration = this.state.configuration;
+            //state.chartTitle = configuration.series[0].name;
+            state.chartTitle = configuration.titles[0].text;
+            state.categoryColumn = configuration.series[0].dataFields.category;
+            state.valueColumn = configuration.series[0].dataFields.value;
+            //IMPORTANT: amCharts pie chart has a bug - it does not display footer in the correct place. Therefore chart footer is not supported.
+            //state.chartFooter = configuration.chartContainer.children[0].text;
+            state.displayToolTip = configuration['cursor'] ? true : false;
+            return state;
+        });
+    }
+    
+    stateToGraphConfiguration = () => {
+        let configuration = this.state.configuration;
+        configuration.series[0].name = this.state.chartTitle;
+        configuration.titles[0].text = this.state.chartTitle;
+        configuration.series[0].dataFields.category = this.state.categoryColumn;
+        configuration.series[0].dataFields.value = this.state.valueColumn;
+        //IMPORTANT: amCharts pie chart has a bug - it does not display footer in the correct place. Therefore chart footer is not supported.
+        //configuration.chartContainer.children[0].text = this.state.chartFooter;
+        if (this.state.displayToolTip) {
+            configuration.series[0].slices.template.tooltipText = "{name}:[bold]{category} - {value}[/]";
+        }
+        else {
+            configuration.series[0].slices.template.tooltipText = '';
+        }
+    }
+
+    isUserInputValid = (field, value) => {
+        if (value && (typeof value === 'string')) {
+            value = value.trim();
+        }
+        let errorMessage = null;
+        switch(field) {
+            case 'chartTitle':
+                if (!value || ('' === value)) {
+                    errorMessage = 'Chart title is needed';
+                }
+            break;
+
+            case 'categoryColumn':
+                if (!value || ('' === value)) {
+                    errorMessage = 'Category series column is needed';
+                }
+            break;
+
+            case 'valueColumn':
+                if (!value || ('' === value)) {
+                    errorMessage = 'Value series column is needed';
+                }
+            break;
+        }
+        this.setErrorMessage(field, errorMessage);
+        console.debug(`Input field '${field}' with value '${value}' valid? ` + !errorMessage);
+        return !errorMessage;
+    }
+
     render() {
+        let thiz = this;
+        function dataSetColumns() {
+            let options = [];
+            thiz.state.dataSetColumns.map((item, key) => {
+                options.push(
+                    <option value={item.value}>{item.label}</option>
+                );
+            });
+            return options;
+        }
+
         return (
             <>
                 <div className="form-group col">
-                    <div className="card" id="chartProperty">
+                    <div className="card" id="chartPropertyBox">
                         <div className="card-header">
                             Pie chart properties
                         </div>
@@ -24,17 +95,17 @@ class PieChart extends BaseChart {
                                             ((!this.state.align || this.state.align === 'left' || this.state.align === '') ? ' active' : '')}>
                                             <input type="radio" name="align" value="left" autoComplete="off" 
                                                 checked={this.state.align ? ((this.state.align === 'left') || (this.state.align === '')) : false} 
-                                                onChange={this.selectionChanged} disabled={this.state.readOnly}/> Left
+                                                onChange={this.selectionChanged}/> Left
                                         </label>
                                         <label className={'btn btn-info' + ((this.state.align === 'center') ? ' active' : '')}>
                                             <input type="radio" name="align" value="center" autoComplete="off" 
-                                                checked={this.state.align ? (this.state.align === 'center') : false} onChange={this.selectionChanged} 
-                                                disabled={this.state.readOnly}/> Center
+                                                checked={this.state.align ? (this.state.align === 'center') : false} 
+                                                onChange={this.selectionChanged}/> Center
                                         </label>
                                         <label className={'btn btn-info' + ((this.state.align === 'right') ? ' active' : '')}>
                                             <input type="radio" name="align" value="right" autoComplete="off" 
-                                                checked={this.state.align ? (this.state.align === 'right') : false} onChange={this.selectionChanged} 
-                                                disabled={this.state.readOnly}/> Right
+                                                checked={this.state.align ? (this.state.align === 'right') : false} 
+                                                onChange={this.selectionChanged}/> Right
                                         </label>
                                     </div>
                                 </div>
@@ -47,6 +118,7 @@ class PieChart extends BaseChart {
                                         value={this.state.chartTitle ? this.state.chartTitle : ''}/>
                                 </div>
                             </div>
+                            { /* IMPORTANT: amCharts pie chart has a bug - it does not display footer in the correct place. Therefore chart footer is not supported.
                             <div className="form-group row">
                                 <label htmlFor="chartFooter" className="col-4 col-form-label form-control-sm">Chart footer</label>
                                 <div className="col-8">
@@ -55,16 +127,43 @@ class PieChart extends BaseChart {
                                         value={this.state.chartFooter ? this.state.chartFooter : ''}/>
                                 </div>
                             </div>
+                            */ }
                             <div className="form-group row">
+                                <label htmlFor="categoryColumn" className="col-4 col-form-label form-control-sm">Category series column</label>
+                                <div className="col-8">
+                                    <select id="categoryColumn" name="categoryColumn" ref="categoryColumn" className="form-control form-control-sm" 
+                                        onChange={this.selectionChanged} disabled={this.state.readOnly} 
+                                        value={this.state.categoryColumn ? this.state.categoryColumn : ''}>
+                                        <option value="">-Select category column-</option>
+                                        { dataSetColumns() }
+                                    </select>
+                                    <Overlay target={this.refs.categoryColumn} show={this.state.errors.categoryColumn != null} placement="left">
+                                        {props => (
+                                        <Tooltip id="categoryColumn-tooltip" {...props} className="error-tooltip">
+                                            {this.state.errors.categoryColumn}
+                                        </Tooltip>
+                                        )}
+                                    </Overlay>
+                                </div>
                             </div>
                             <div className="form-group row">
+                                <label htmlFor="valueColumn" className="col-4 col-form-label form-control-sm">Value series column</label>
+                                <div className="col-8">
+                                    <select id="valueColumn" name="valueColumn" ref="valueColumn" className="form-control form-control-sm" 
+                                        onChange={this.selectionChanged} disabled={this.state.readOnly} 
+                                        value={this.state.valueColumn ? this.state.valueColumn : ''}>
+                                        <option value="">-Select value column-</option>
+                                        { dataSetColumns() }
+                                    </select>
+                                    <Overlay target={this.refs.valueColumn} show={this.state.errors['valueColumn'] != null} placement="left">
+                                        {props => (
+                                        <Tooltip id="valueColumn-tooltip" {...props} className="error-tooltip">
+                                            {this.state.errors['valueColumn']}
+                                        </Tooltip>
+                                        )}
+                                    </Overlay>
+                                </div>
                             </div>
-                            <div className="form-group row">
-                            </div>
-                            <div className="form-group row">
-                            </div>
-                            { /* Begin add value series. */ }
-                            { /* End add value series. */ }
                             <div className="form-group row">
                                 <label className="form-check-label col-4 col-form-label form-control-sm" htmlFor="displayToolTip">Display tool-tip</label>
                                 <div className="col-8">
@@ -76,7 +175,7 @@ class PieChart extends BaseChart {
                     </div>
                 </div>
                 <div className="form-group col">
-                    <div className="card">
+                    <div className="card" id="chartPreviewBox">
                         <div className="card-header">
                             Preview
                         </div>
