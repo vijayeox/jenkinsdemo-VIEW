3fbaa025543849503bfaa30ef2823cb09f3d9653
bharatg <bharatg@myvamla.com>
2019-11-22 21:10:13 +0530

    Clean up UI in apps

--- gui/src/components/App/Page.js
+++ gui/src/components/App/Page.js
@@ -323,7 +323,7 @@ class Page extends React.Component {
     }
     return (
       <div className="loaderAnimation">
-        <Loader type="Circles" color="#00BFFF" height={100} width={100} />
+        <Loader type="Circles" color="#275362" height={100} width={100} />
       </div>
     );
   }
