d103e38ac9563006a8594a6a67721edb94ea7567
danishsultan <danishsultan007@gmail.com>
2019-12-12 20:53:34 +0530

    added hub build script, hubentrypoint for view and updated deploy script

---
+++ hubentrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd bos
+npm rebuild node-sass
+cd ..
+./clean.sh
+./build.sh gui
+./build.sh iconpacks
+./build.sh themes
+./build.sh apps "Admin, Preferences"
+./build.sh bos
+echo build completed successfully
