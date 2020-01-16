2003a24501e9007b9034847e6cf06d9273f92747
bharatg <bharatg@myvamla.com>
2020-01-17 02:26:12 +0530

    Search Page UI Cleanup

--- gui/src/components/App/SearchPage.js
+++ gui/src/components/App/SearchPage.js
@@ -86,6 +86,7 @@ class SearchPage extends React.Component {
     render() {
         return (
             <div>
+            <div className="searchPageDiv">
             <input
             className="searchInput"
             placeholder="Search for..."
@@ -99,6 +100,9 @@ class SearchPage extends React.Component {
                 </div>
                 </div> : null
             }
+            <div className="seperator"></div>
+            <svg className="icon" viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
+            </div>
             {this.search.value != ''?
             <div className="row">
             <div className="col-md-12">
