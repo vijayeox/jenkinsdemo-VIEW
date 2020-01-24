67286c42bf46482279c325a8f990959fa1980395
bharatg <bharatg@myvamla.com>
2020-01-24 12:52:24 +0530

    Search Page UI Cleanup

--- gui/src/components/App/SearchPage.js
+++ gui/src/components/App/SearchPage.js
@@ -85,23 +85,23 @@ class SearchPage extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="searchResults">
             <div className="searchPageDiv">
+            <svg className="icon" viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
             <input
             className="searchInput"
             placeholder="Search for..."
             ref={input => this.search = input}
             onChange={this.handleInputChange} />
+            <div className="seperator"></div>
             {
                 this.search.value?
-                <div className="row">
+                <div className="row searchText">
                 <div className="col-md-12">
                 Search Results for "{this.search.value}"
                 </div>
                 </div> : null
             }
-            <div className="seperator"></div>
-            <svg className="icon" viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
             </div>
             {this.search.value != ''?
             <div className="row">
