62eeadef1d0e45d9360ac0e35abd768463b0c803
ravi <ravi@firespike.in>
2020-01-30 07:46:43 +0530

    Widget to back end integration changes.

--- gui/src/WidgetRenderer.js
+++ gui/src/WidgetRenderer.js
@@ -6,6 +6,7 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import am4themes_kelly from "@amcharts/amcharts4/themes/animated";
+import WidgetTransformer from './WidgetTransformer';
 am4core.useTheme(am4themes_animated);
 
 class WidgetRenderer {
@@ -72,6 +73,31 @@ class WidgetRenderer {
     }
 
     static renderAmCharts(element, configuration, data) {
+        function getAvailablePropertiesAsObject(dataItem, propertyList) {
+            let obj = {};
+            propertyList.forEach(function(prop, index, array) {
+                if (dataItem[prop]) {
+                    obj[prop] = dataItem[prop];
+                }
+            }
+            return obj;
+        }
+
+        function dumpProperties(obj) {
+            for (var prop in obj) {
+                console.log(prop + ' => ' + obj[prop]);
+            }
+            //Object.keys(obj).forEach(function(key,index) {
+            //    // key: the name of the object key
+            //    // index: the ordinal position of the key within the object 
+            //    console.log(key + ' => ' + obj[key]);
+            //});
+        }
+
+        var transformedConfig = WidgetTransformer.transform(configuration, data);
+        configuration = transformedConfig.chartConfiguration;
+        data = transformedConfig.chartData;
+
         let series = configuration.series;
         if (!Array.isArray(series)) {
             throw 'Chart series should be array.';
@@ -79,6 +105,55 @@ class WidgetRenderer {
         if (0 === series.length) {
             throw 'Chart series is empty.';
         }
+
+        series.forEach(function(ser) {
+            switch(ser.type) {
+                case 'ColumnSeries':
+                    if (!ser['columns']) {
+                        ser['columns'] = {};
+                    }
+                    let columns = ser['columns'];
+                    if (!columns['events']) {
+                        columns['events'] = {};
+                    }
+                    let  seriesEvts = columns['events'];
+                     seriesEvts['hit'] = function(evt) {
+                        let dataObj = getAvailablePropertiesAsObject(evt.target.dataItem, [
+                            'valueX',
+                            'valueY',
+                            'dateX',
+                            'dateY',
+                            'categoryX',
+                            'categoryY',
+                            'openValueX',
+                            'openValueY',
+                            'openDateX',
+                            'openDateY',
+                            'openCategoryX',
+                            'openCategoryY'
+                        ]);
+                        //console.log("Clicked => category:" + evt.target.dataItem.categoryX + ", value:" + evt.target.dataItem.valueY);
+                        //console.log("Clicked => ", evt.target.dataItem);
+                        console.log("Clicked => ", dataObj);
+                    }
+                break;
+                case 'LineSeries':
+                    if (!ser['bullets']) {
+                        ser['bullets'] = {};
+                    }
+                    let bullets = ser['bullets'];
+                    if (!bullets['events']) {
+                        bullets['events'] = {};
+                    }
+                    let bulletEvts = bullets['events'];
+                    bulletEvts['hit'] = function(evt) {
+                        console.log("Clicked => ", evt.target.dataItem);
+                        dumpProperties(evt.target.dataItem);
+                    }
+                break;
+            }
+        });
+
         let type = series[0].type;
         let am4ChartType;
         switch(type) {
