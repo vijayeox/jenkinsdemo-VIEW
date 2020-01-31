60c8f08c108afb61523af45e1c1de8d3bfcb0e23
nehar <neha@myvamla.com>
2020-01-31 10:44:34 +0530

    Merge Issue from QA in Login

--- bos/src/client/pages/Login.js
+++ bos/src/client/pages/Login.js
@@ -475,35 +475,6 @@ export default class Login extends defaultLogin {
             h("div", { className: "footer-links" }, [
               h("a", { href: "https://www.vantageagora.com" }, "About Us")
             ]),
-    const view = (state,actions) => 
-    h("main",{id: 'login-container ', className: 'loginContainer row lighten-3 '},[
-      h('div',{id: 'ox-login-form', className: 'form-wrapper'},[
-      h('div',{ className: 'form-wrapper__inner'},[
-        h("form",{action:"#",className: 'form-signin form-row-layout',loading: false,method: "post",onsubmit: actions.submit, className:'ox-form '},[
-          h('div',{id: 'ox-img', className: 'ox-imgDiv'},[
-            h('img',{id:'ox-logo', className: 'ox-img',src:require('../assets/images/eox.png')}),
-            ]),
-            h('div',{className: 'floating-label'},[
-            h("input",{type: "text",name:"username",className:'validate',id:'username', placeholder:"Username"}),
-            h('label',{for:'username'},'Username')
-          ]),
-          h('div',{className: 'floating-label'},[
-            h("input",{type: "password",name:"password",className:'validate',id:'password', placeholder:"Password"}),
-            h('label',{for:'password'},'Password')
-          ]),
-          h('div', {
-            class: 'osjs-login-error',
-            style: {display: state.error ? 'block' : 'none'}
-          }, h('span', {}, "The username and/or password is incorrect! Please try again.")),
-          h('div',{className: 'form-signin__footer'},[
-          h("button",{type:"submit",value:"login",className: 'btn waves-effect waves-light'},'Login'),
-          h('a',{href: '#'},'Forgot your password?'),
-        ]),
-        ]),
-      ]),
-      h('div',{className:'footer-links'},[
-        h('a',{href:'https://eoxvantage.com',className:'footer-link', target:'_blank'},'About Us'),
-      ]),
       h('div',{className:'login-copyright'},'Copyright © 2020 EOX Vantage. All rights reserved.'),
       ])
     ])
