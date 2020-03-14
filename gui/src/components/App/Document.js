1e5fa34c242679040ff08fe13c1dd1d7ba28f940
prajwal <prajwals@myvamla.com>
2020-03-15 02:13:21 +0530

    Add JSX parser instead of settingInnerHtml Fix timing issue, Remove redundant replace Params function generalize parseFileData func

--- gui/src/components/App/Document.js
+++ gui/src/components/App/Document.js
@@ -1,4 +1,5 @@
 import React from "react";
+import JsxParser from "react-jsx-parser";
 
 class Document extends React.Component {
   constructor(props) {
@@ -6,13 +7,12 @@ class Document extends React.Component {
     this.config = this.props.config;
     this.args = this.props.args;
     this.core = this.props.core;
-    this.profileAdapter = this.core.make('oxzion/profile');
-    this.profile = this.profileAdapter.get()
+    this.profileAdapter = this.core.make("oxzion/profile");
+    this.profile = this.profileAdapter.get();
     this.fileId = this.props.fileId;
     this.appId = this.props.appId;
     this.state = {
       content: this.props.content,
-      fileData: {},
       dataReady: false
     };
   }
@@ -21,25 +21,24 @@ class Document extends React.Component {
       this.getFileDetails(this.fileId).then(response => {
         if (response.status == "success") {
           this.setState({
-            fileData: response.data.data,
+            content: this.parseFileData(this.state.content, response.data.data),
             dataReady: true
           });
-          this.setState({
-            content: this.parseFileData(this.state.content)
-          })
-        } else {
-          this.setState({ fileData: [] });
         }
       });
-    }
-    else {
+    } else {
       this.setState({
-        content: this.replaceParams(this.state.content,this.profile.key)
-
-      })
-      console.log(this.profile)
+        content: this.parseFileData(this.state.content, this.profile.key)
+      });
     }
   }
+
+  parseFileData(string, fileData) {
+    return string.replace(/{{\s*([^}]*)\s*}}/g, function(m, $1) {
+      return fileData[$1];
+    });
+  }
+
   async getFileDetails(fileId) {
     // call to api using wrapper
     let helper = this.core.make("oxzion/restClient");
@@ -52,43 +51,6 @@ class Document extends React.Component {
     return fileContent;
   }
 
-  parseFileData(string) {
-    console.log(this.state.fileData);
-    var that = this;
-    if (this.state.fileData) {
-      return string.replace(/{{\s*([^}]*)\s*}}/g, function(m, $1) {
-        return that.state.fileData[$1];
-      });
-    } else{
-
-    }
-  }
-
-  replaceParams(route, params) {
-    if (!params) {
-      return route;
-    }
-    var regex = this.literalExpressionRegex();
-    let m;
-    while ((m = regex.exec(route)) != null){
-      // This is necessary to avoid infinite loops with zero-width matches
-      if (m.index == regex.lastIndex) {
-        regex.lastIndex++;
-      }
-      // The result can be accessed through the `m`-variable.
-      m.forEach((match, groupIndex) => {
-        // console.log(`Found match, group ${groupIndex}: ${match}`);
-        route = route.replace(match, params[match.replace(/\{\{|\}\}/g, "")]);
-      });
-      regex = this.literalExpressionRegex();
-    }
-    return route;
-  }
-
-  literalExpressionRegex = () => {
-    return /\{\{.*?\}\}/g;
-  } 
-  
   componentDidUpdate(prevProps) {
     if (this.props.content !== prevProps.content) {
       this.setState({ content: this.props.content });
@@ -97,9 +59,7 @@ class Document extends React.Component {
 
   render() {
     return this.fileId ? (
-      this.state.dataReady && (
-        <div dangerouslySetInnerHTML={{ __html: this.state.content }} />
-      )
+      this.state.dataReady && <JsxParser jsx={this.state.content} />
     ) : (
       <div dangerouslySetInnerHTML={{ __html: this.state.content }} />
     );
