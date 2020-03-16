f8782398f7a14fb1e3f203b86ff6d1d62b8808ac
bharatg <bharatg@myvamla.com>
2020-03-16 11:26:01 +0530

    Document Viewer component bug fixes

--- gui/src/components/App/Form/DocumentViewerComponent.js
+++ gui/src/components/App/Form/DocumentViewerComponent.js
@@ -3,13 +3,14 @@ import editForm from 'formiojs/components/table/Table.form';
 
 export default class DocumentViewerComponent extends Base {
   constructor(component, options, data) {
+    super(component, options, data);
     component.core = null;
     component.appId = null;
     component.uiUrl = null;
+    this.form = this.getRoot();
+    var that = this;
     component.wrapperUrl = '<p> No Files to Display</p>';
-    super(component, options, data);
-    
-    window.addEventListener('appDetails', function (e) {
+    that.form.element.addEventListener('appDetails', function (e) {
       component.core = e.detail.core;
       component.appId = e.detail.appId;
       component.uiUrl = e.detail.uiUrl;
@@ -32,47 +33,65 @@ export default class DocumentViewerComponent extends Base {
     weight: 70,
     schema: DocumentViewerComponent.schema()
   }
-  onChange(flags, fromRoot){
-    var dataValue = this.dataValue;
-    if(typeof dataValue == 'string'){
+    onChange(flags, fromRoot){
+      var dataValue = this.dataValue;
+      if(typeof dataValue == 'string'){
        dataValue = JSON.parse(dataValue);
-    }
-    this.fileList = this.getFileList(dataValue);
-    var component = this.component
-    this.redraw();
-    if(dataValue && dataValue != undefined){
-      var evt = new CustomEvent("getAppDetails", {detail:{} });
-      window.dispatchEvent(evt);
-      for (var prop in dataValue) {
-        if(document.getElementById('selectFile_'+prop)){
-          document.getElementById('selectFile_'+prop).addEventListener("click",function(event){
-            var url;
-            var fileUrl = dataValue[prop];
-            var type = fileUrl.substr(fileUrl.lastIndexOf('.') + 1);
-            if (type == "pdf") {
-              url = component.uiUrl+"/ViewerJS/#" + component.wrapperUrl+component.appId+'/'+fileUrl;
-            } else if (type == "image") {
-              url = documentData;
-            } else {
-              url = component.uiUrl+"/ViewerJS/#" + component.wrapperUrl+component.appId+'/'+fileUrl;
-            }
-            console.log(url);
-            document.getElementById('displayDocumentData').innerHTML = '<iframe src="'+url+'" class="iframeDoc" key="'+url+'"></iframe>';
-            event.stopPropagation();
-          },true);
-        }
-      }
+     }
+     var component = this.component
+     var that =this;
+     var evt = new CustomEvent("getAppDetails", {detail:{} });
+     this.form.element.dispatchEvent(evt);
+     this.fileList = this.getFileList(dataValue,component);
+     this.redraw();
+     if(dataValue && dataValue != undefined){
+      var elements = document.getElementsByClassName("selectFile");
+      Array.from(elements).forEach(function(element) {
+        element.addEventListener('click', function(event){
+          var file = this.parentElement.parentElement.getAttribute("data-file");
+
+          var url = this.parentElement.getAttribute("data-url");
+          if(!url){
+            url = this.parentElement.parentElement.getAttribute("data-url");
+          }
+          document.getElementById('filePreviewModal').style.display = "block";
+          document.getElementById('filePreviewModal').style.height = that.form.element.parentElement.parentElement.clientHeight + 'px';
+          document.getElementById('filePreviewWindow').innerHTML = '<iframe src="'+url+'" allowTransparency="true" frameborder="0" scrolling="yes" style="width:100%;height:100%;" class="iframeDoc" key="'+url+'"></iframe>';
+          event.stopPropagation();
+        });
+      });
+      var downloadElements = document.getElementsByClassName("downloadFile");
+      Array.from(downloadElements).forEach(function(element) {
+        element.addEventListener('click', function(event){
+          var url = this.parentElement.parentElement.getAttribute("data-downloadurl");
+          window.open(url,'_blank');
+          event.stopPropagation();
+        });
+      });
+      document.getElementById('closeFile').addEventListener('click', function(event){
+        document.getElementById('filePreviewModal').style.display='none'
+      });
     }
   }
-  getFileList(files){
+  getFileList(files,component){
     var fileList = '';
     if(files){
       for (var prop in files) {
-        fileList += `<div class="card docList" key="`+prop+`">
-        <div class="card-body docListBody border" id="selectFile_`+prop+`">
-       <br></br>`+files[prop].substring(files[prop].lastIndexOf('/')+1)+`
-        </a>
-        </div>
+        var type = files[prop].substr(files[prop].lastIndexOf('.') + 1);
+        var url = '';
+        var icon = "<i class='fa fa-file-o'></i>";
+        if (type == "pdf") {
+          url = component.uiUrl+"/ViewerJS/#" + component.wrapperUrl+component.appId+'/'+files[prop];
+          icon = "<i class='fa fa-file-pdf-o'></i>";
+        } else if (type == "image") {
+          url = documentData;
+          icon = "<i class='fa fa-picture-o'></i>";
+        } else {
+          url = component.uiUrl+"/ViewerJS/#" + component.wrapperUrl+component.appId+'/'+files[prop];
+        }
+        fileList += `<div class="docList" key="`+prop+`">
+        <div class="fileDiv"><div class="singleFile row" id="selectFile_`+prop+`" data-downloadurl="`+component.wrapperUrl+component.appId+'/'+files[prop]+`" data-file="`+prop+`" data-url="`+url+`"><span class="col-md-11 selectFile" style="line-height: 1.5; vertical-align: middle;padding: .375rem .75rem;">`+icon+' '+files[prop].substring(files[prop].lastIndexOf('/')+1)+`</span><span class="col-md-1"><button class="btn btn-sm btn-info selectFile" ><i class="fa fa-eye"></i></button><button class="btn btn-sm btn-info downloadFile" style="margin-left:5px;" ><i class="fa fa-download"></i></button>
+        </span></div></div>
         </div>`;
       }
     } else {
@@ -82,11 +101,12 @@ export default class DocumentViewerComponent extends Base {
   }
 
   render(children) {
-    var row = `<div class="row" style="height: -webkit-fill-available" >
-    <div class="col-md-2 docListDiv" >`+ this.fileList +`</div>
-    <div class="col-md-10 border" id="displayDocumentData">
+    var fileList = this.fileList?this.fileList:null;
+    var row = `<div class="row" style="padding: 15px;" >
+    <div class="col-md-12" >`+ fileList +`</div>
     </div>
-    </div>`;
+    </div><div id="filePreviewModal" class="modal" style="position:absolute"><div style="height:inherit;display: block;background-color: white;"><span id="closeFile" class="close" style="font-size:2em">&times;</span><div id="filePreviewWindow" style="height:inherit"></div></div></div>`;
+    console.log(row);
     var content = super.render(row);
     return content;
   }
