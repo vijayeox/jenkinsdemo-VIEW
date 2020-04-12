e1d87fe24bf7c89d22536413ef62b824bcbbf0d3
bharatg <bharatg@myvamla.com>
2020-04-13 01:38:09 +0530

    Document Viewer Issue

--- gui/src/components/App/Form/DocumentViewerComponent.js
+++ gui/src/components/App/Form/DocumentViewerComponent.js
@@ -189,11 +189,11 @@ export default class DocumentViewerComponent extends Base {
           disableView = true;
         }
         fileList +=
-          `<div class="docList" key="` +
+          `<div class="docList" style="margin:0;" key="` +
           prop +
           `">
            <div class="fileDiv">
-          <div class="singleFile" ` +
+          <div class="singleFile row" ` +
           prop +
           `" data-downloadurl="` +
           downloadUrl +
@@ -204,7 +204,7 @@ export default class DocumentViewerComponent extends Base {
           `" data-url="` +
           url +
           `">
-            <span class="col-md-10 ` +
+            <span class="fileName col-md-10 ` +
           component.key +
           `-downloadFile">` +
           icon +
