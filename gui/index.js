3b49a5c16b81de0d001d85f6a7ac45c868e7e40a
Prajwal K <prajwals@eoxvantage.in>
2020-04-21 15:56:22 +0530

    Added CommentsView gui component Added react-mentions package

--- gui/index.js
+++ gui/index.js
@@ -5,6 +5,7 @@ import MultiSelect from "./src/MultiSelect";
 import FileUploader from "./src/FileUploader";
 import FormRender from "./src/components/App/FormRender";
 import HTMLViewer from "./src/components/App/HTMLViewer";
+import CommentsView from "./src/components/App/CommentsView";
 import OX_Grid from "./src/OX_Grid";
 import DocumentViewer from "./src/DocumentViewer";
 import Dashboard from "./src/Dashboard";
@@ -34,6 +35,7 @@ export {
   MultiSelect,
   FileUploader,
   HTMLViewer,
+  CommentsView,
   FormRender,
   OX_Grid,
   DocumentViewer,
