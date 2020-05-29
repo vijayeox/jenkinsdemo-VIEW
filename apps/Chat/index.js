309febfd558d00b47ce1c28ffea25b7575452918
Sadhitha <saditha@myvamla.com>
2020-05-29 13:44:26 +0530

    Reverted one change - clearusercookie - Clearing particular cookie

--- apps/Chat/index.js
+++ apps/Chat/index.js
@@ -46,14 +46,8 @@
 
   // To clear Client-side cookie
   const clearClientCookie = () => {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        console.log(name);
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
+    document.cookie = 'MMUSERID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/';
+    document.cookie = `MMUSERID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=${window.location.hostname};path=/`;
   };
 
   // Chat Header Icons are interchanged purposefully. Do not change this.
