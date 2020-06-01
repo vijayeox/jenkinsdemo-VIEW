1271a8cd4af51935af016ca96b4a042a0abfbcf6
Prajwal K <prajwals@eoxvantage.in>
2020-06-01 21:53:13 +0530

    Add default prop value for filterOperators in OX_Grid

--- gui/src/OX_Grid.js
+++ gui/src/OX_Grid.js
@@ -500,13 +500,49 @@ class DetailComponent extends GridDetailRow {
 
 OX_Grid.defaultProps = {
   data: [],
-  scrollable: "scrollable"
+  scrollable: "scrollable",
+  filterOperators: {
+    text: [
+      { text: "grid.filterStartsWithOperator", operator: "startswith" },
+      { text: "grid.filterContainsOperator", operator: "contains" },
+      { text: "grid.filterEqOperator", operator: "eq" },
+      { text: "grid.filterNotContainsOperator", operator: "doesnotcontain" },
+      { text: "grid.filterNotEqOperator", operator: "neq" },
+      { text: "grid.filterEndsWithOperator", operator: "endswith" },
+      { text: "grid.filterIsNullOperator", operator: "isnull" },
+      { text: "grid.filterIsNotNullOperator", operator: "isnotnull" },
+      { text: "grid.filterIsEmptyOperator", operator: "isempty" },
+      { text: "grid.filterIsNotEmptyOperator", operator: "isnotempty" },
+    ],
+    numeric: [
+      { text: "grid.filterEqOperator", operator: "eq" },
+      { text: "grid.filterNotEqOperator", operator: "neq" },
+      { text: "grid.filterGteOperator", operator: "gte" },
+      { text: "grid.filterGtOperator", operator: "gt" },
+      { text: "grid.filterLteOperator", operator: "lte" },
+      { text: "grid.filterLtOperator", operator: "lt" },
+      { text: "grid.filterIsNullOperator", operator: "isnull" },
+      { text: "grid.filterIsNotNullOperator", operator: "isnotnull" },
+    ],
+    date: [
+      { text: "grid.filterEqOperator", operator: "eq" },
+      { text: "grid.filterNotEqOperator", operator: "neq" },
+      { text: "grid.filterAfterOrEqualOperator", operator: "gte" },
+      { text: "grid.filterAfterOperator", operator: "gt" },
+      { text: "grid.filterBeforeOperator", operator: "lt" },
+      { text: "grid.filterBeforeOrEqualOperator", operator: "lte" },
+      { text: "grid.filterIsNullOperator", operator: "isnull" },
+      { text: "grid.filterIsNotNullOperator", operator: "isnotnull" },
+    ],
+    boolean: [{ text: "grid.filterEqOperator", operator: "eq" }],
+  },
 };
 
 OX_Grid.propTypes = {
   data: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
   checkBoxSelection: PropTypes.func,
   columnConfig: PropTypes.array.isRequired,
+  filterOperators: PropTypes.object,
   gridDefaultFilters: PropTypes.object,
   gridToolbar: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   gridNoRecords: PropTypes.element,
