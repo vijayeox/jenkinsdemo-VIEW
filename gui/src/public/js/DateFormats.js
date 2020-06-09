816d2885a94f0f86cb5f2efd8dd58a1d3ad2bae4
Prajwal K <prajwals@eoxvantage.in>
2020-06-10 02:44:23 +0530

    Fix invalid date formats in gui/DateFormats.js

--- gui/src/public/js/DateFormats.js
+++ gui/src/public/js/DateFormats.js
@@ -1,18 +1,18 @@
 export default [
   {
-    label: "19-06-2019 (dd-MM-yyyy)",
-    value: "dd-MM-yyyy"
+    label: "19-06-2019 (DD-MM-YYYY)",
+    value: "DD-MM-YYYY"
   },
   {
-    label: "19-Jun-2019 (dd-MMM-yyyy)",
-    value: "dd-MMM-yyyy"
+    label: "06-19-2019 (MM-DD-YYYY)",
+    value: "MM-DD-YYYY"
   },
   {
-    label: "2019-06-19 (yyy-MM-dd)",
-    value: "yyy-MM-dd"
+    label: "19-06-2019 (DD-MM-YYYY)",
+    value: "DD-MM-YYYY"
   },
   {
-    label: "06-19-2019 (MM-dd-yyyy)",
-    value: "MM-dd-yyyy"
+    label: "June 19th 2019 (MMMM Do YYYY)",
+    value: "MMMM Do YYYY"
   }
 ];
