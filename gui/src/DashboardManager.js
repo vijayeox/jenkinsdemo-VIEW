06c1b5e394a93319d40aef2890044d44e29bc8f0
rakshith@vantageagora.com <rakshith@vantageagora.com>
2020-07-09 18:46:04 +0530

    @Bug: 11018 - Fixed the default Dashboard showing in all the page issue

--- gui/src/DashboardManager.js
+++ gui/src/DashboardManager.js
@@ -266,15 +266,19 @@ class Dashboard extends React.Component {
                                 <i className="fa fa-trash" aria-hidden="true"></i>
                               </Button>
                             }
-                            {!this.props.hideEdit && this.userProfile.key.privileges.MANAGE_DASHBOARD_WRITE &&
+                            {this.userProfile.key.privileges.MANAGE_DASHBOARD_WRITE &&
                               (this.state.inputs["dashname"] != undefined && this.state.inputs["dashname"]["isdefault"] == "0") ?
-                              <Button
-                                onClick={() => this.dashboardOperation(this.state.inputs["dashname"], "SetDefault")}
-                                title="Make current dashboard as default dashboard"
-                              >
-                                MAKE DEFAULT
+                              (this.props.hideEdit == false &&
+                                <Button
+                                  onClick={() => this.dashboardOperation(this.state.inputs["dashname"], "SetDefault")}
+                                  title="Make current dashboard as default dashboard"
+                                >MAKE DEFAULT
                                 </Button>
-                              : <span style={{ color: "white", fontWeight: "bolder" }}>Default Dashboard</span>
+                              )
+                              : (this.props.hideEdit == false &&
+                                <span style={{ color: "white", fontWeight: "bolder" }}>Default Dashboard 123</span>
+                              )
+
                             }
                           </>
                         }
