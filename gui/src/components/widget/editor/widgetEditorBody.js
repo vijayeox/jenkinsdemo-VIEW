a8745a7e5ad3289885eed0f42c633871d0251002
rakshith@vantageagora.com <rakshith@vantageagora.com>
2020-07-09 13:33:06 +0530

    @Bug: 10731 - Changed the path to the WidgetRenderer page

--- gui/src/components/widget/editor/widgetEditorBody.js
+++ gui/src/components/widget/editor/widgetEditorBody.js
@@ -1,4 +1,4 @@
-import { WidgetRenderer } from '../../../GUIComponents';
+import { WidgetRenderer } from '../../../WidgetRenderer';
 import AbstractEditor from './abstractEditor';
 import React from 'react';
 import ReactDOM from 'react-dom';
