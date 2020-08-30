52fe14ad40c046e770535c0e3f71c2f8d715797e
Prajwal K <prajwals@eoxvantage.in>
2020-08-31 01:53:15 +0530

    Add click event listener on form next button to scroll to top of the page when there are errors

--- gui/src/components/App/FormRender.js
+++ gui/src/components/App/FormRender.js
@@ -839,9 +839,32 @@ class FormRender extends React.Component {
                 }
               }
             },true);
-            var elm = document.getElementsByClassName(that.state.appId + "_breadcrumbParent");
-            if (elm.length > 0) {
-              scrollIntoView(elm[0], { scrollMode: "if-needed",block: "center",behavior: "smooth",inline: "nearest" });
+            var nextButton = document.getElementsByClassName(
+              "btn-wizard-nav-next"
+            );
+            var elm = document.getElementsByClassName(
+              that.state.appId + "_breadcrumbParent"
+            );
+            if (nextButton.length > 0) {
+             // check to make sure event is appended only once
+              if (
+                nextButton[0].getAttribute("errorScrollEventAdded") !== "true"
+              ) {
+                nextButton[0].setAttribute("errorScrollEventAdded", "true");
+                nextButton[0].addEventListener("click", () => {
+                  var submitErrors = [
+                    ...document.querySelectorAll('[ref="errorRef"]')
+                  ];
+                  if (elm.length > 0 && submitErrors.length > 0) {
+                    scrollIntoView(elm[0], {
+                      scrollMode: "if-needed",
+                      block: "center",
+                      behavior: "smooth",
+                      inline: "nearest"
+                    });
+                  }
+                });
+              }
             }
             if (that.state.formLevelDelegateCalled == false) {
               that.setState({formLevelDelegateCalled: true});
