671f92b69d1b50cf0808185baaaf5aa33ef98aa5
Prajwal K <prajwals@myvamla.com>
2020-10-16 04:26:21 +0530

    [REFACTOR] Add excel export file name Wrap OX_Grid in custom classname Change componentWillReceiveProps prop comparison logic

--- gui/src/OX_Grid.js
+++ gui/src/OX_Grid.js
@@ -23,6 +23,7 @@ import DataOperation from "./components/Grid/DataOperation";
 import CustomFilter from "./components/Grid/CustomFilter";
 import "./components/Grid/customStyles.scss";
 import InlineComponent from "./components/Grid/InlineComponent";
+const util = require('util');
 
 export default class OX_Grid extends React.Component {
   constructor(props) {
@@ -40,7 +41,7 @@ export default class OX_Grid extends React.Component {
         ? this.props.gridDefaultFilters
         : {},
       apiActivityCompleted: this.rawDataPresent ? true : false,
-      isTab: this.props.isTab ? this.props.isTab : false
+      isTab: this.props.isTab ? this.props.isTab : false,
     };
     this.appNavigationDiv = "navigation_" + this.props.appId;
     this.loader = this.props.osjsCore.make("oxzion/splash");
@@ -86,20 +87,20 @@ export default class OX_Grid extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     // change to use componentDidUpdate later in future
-    const circularReplacer = (obj) => {
-      var i = 0;
-      return function(key, value) {
-        if(i !== 0 && typeof(obj) === 'object' && typeof(value) == 'object' && obj == value) 
-          return '[Circular]'; 
-        if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
-          return '[Unknown]';
-        ++i; // so we know we aren't using the original object anymore
-        return value;  
-      }
-    }
-    if (JSON.stringify(this.props, circularReplacer(this.props)) != JSON.stringify(nextProps, circularReplacer(nextProps))) {
+  // Write different props which when changed we need to trigger a setState
+    if (
+      util.inspect(this.props.data, { depth: 2 }) !=
+        util.inspect(nextProps.data) ||
+      util.inspect(this.props.gridDefaultFilters, {depth: 4}) !=
+        util.inspect(nextProps.gridDefaultFilters, {depth: 4}) ||
+      util.inspect(this.props.columnConfig) !=
+        util.inspect(nextProps.columnConfig)
+    ) {
       if (nextProps.gridDefaultFilters) {
-        let mergedFilters = {...nextProps.gridDefaultFilters, ...this.state.dataState}
+        let mergedFilters = {
+          ...this.state.dataState,
+          ...nextProps.gridDefaultFilters,
+        };
         this.setState({ dataState: mergedFilters });
       }
       // Disable untill there is a use case to do this
@@ -312,7 +313,6 @@ export default class OX_Grid extends React.Component {
     if (this.props.exportToExcel) {
       gridToolbarContent.push(
         <Button
-          style={{ right: "10px", float: "right" }}
           primary={true}
           onClick={() => this.exportExcel(this.props.exportToExcel)}
         >
@@ -537,7 +537,8 @@ export default class OX_Grid extends React.Component {
       <div
         style={this.props.wrapStyle ? this.props.wrapStyle : { height: "100%" }}
         className={
-          "GridCustomStyle " + (this.props.className ? this.props.className : "")
+          "GridCustomStyle " +
+          (this.props.className ? this.props.className : "")
         }
       >
         {this.rawDataPresent ? (
@@ -649,7 +650,14 @@ export default class OX_Grid extends React.Component {
           </GridPDFExport>
         ) : null}
         {this.props.exportToExcel ? (
-          <ExcelExport ref={(excelExport) => (this._excelExport = excelExport)}>
+          <ExcelExport
+            ref={(excelExport) => (this._excelExport = excelExport)}
+            fileName={
+              this.props.exportToExcel.fileNameTemplate
+                ? eval(this.props.exportToExcel.fileNameTemplate)
+                : undefined
+            }
+          >
             {this.props.exportToExcel.columnConfig
               ? this.props.exportToExcel.columnConfig.map((item) => (
                   <ExcelExportColumn
