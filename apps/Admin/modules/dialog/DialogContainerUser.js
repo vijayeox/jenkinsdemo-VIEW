00f15ea4f5c934017198674b0f261afc9c697b02
bharatg <bharatg@myvamla.com>
2021-02-17 18:25:25 +0530

    [BUG] Admin not saving Gender

--- apps/Admin/modules/dialog/DialogContainerUser.js
+++ apps/Admin/modules/dialog/DialogContainerUser.js
@@ -453,10 +453,10 @@ export default class DialogContainer extends React.Component {
                           type="radio"
                           id="mRadio"
                           name="gender"
-                          value="Male"
+                          value="male"
                           className="k-radio"
                           onChange={(e) => this.valueChange("gender", e)}
-                          checked={this.state.userInEdit.gender == "Male"}
+                          checked={this.state.userInEdit.gender == "male"}
                           disabled={this.props.diableField ? true : false}
                           required
                         />
@@ -472,10 +472,10 @@ export default class DialogContainer extends React.Component {
                           type="radio"
                           id="fRadio"
                           name="gender"
-                          value="Female"
+                          value="female"
                           className="k-radio pl-2"
                           onChange={(e) => this.valueChange("gender", e)}
-                          checked={this.state.userInEdit.gender == "Female"}
+                          checked={this.state.userInEdit.gender == "female"}
                           disabled={this.props.diableField ? true : false}
                           required
                         />
