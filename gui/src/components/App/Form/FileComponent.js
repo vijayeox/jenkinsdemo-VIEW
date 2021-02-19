e7e903904190a4aac29524dc895fd4d1b4b521f8
bharatg <bharatg@myvamla.com>
2021-02-19 19:07:20 +0530

    [BUG] Upload Documents Error

--- gui/src/components/App/Form/FileComponent.js
+++ gui/src/components/App/Form/FileComponent.js
@@ -150,7 +150,7 @@ export default class FileComponent extends File {
             download(file.url, file.originalName || file.name, file.type);
           }
           else {
-            if(fileInfo.url){
+            if(fileInfo.url && !fileInfo.url.includes(this.component.wrapperUrl+this.component.appId, 0)){
               window.open(fileInfo.url.replace(this.component.uiUrl,this.component.wrapperUrl+this.component.appId), '_blank');
             } else {
               window.open(file.url, '_blank');
