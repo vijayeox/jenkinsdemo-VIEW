a9ea51de9e2e93e97317f184c0631e4c90dd1a3d
vijay <vijay@vantageagora.com>
2021-05-09 20:10:25 +0530

    Removed view

--- apps/Admin/modules/components/index.js
+++
@@ -1,7 +0,0 @@
-import DateComponent from "./DateComponent.js";
-import SaveCancel from "./saveCancel.js";
-import DropDown from "./DropDownList";
-import CurrencySelect from "./Currency Select/currencySelect"
-import FileUploader from "./FileUploader";
-
-export { DateComponent, SaveCancel, DropDown, CurrencySelect, FileUploader };
--- gui/test/GUITestApp/GUIComponents.js
+++
@@ -1,4 +0,0 @@
-import OX_Grid from "OxzionGUI/OX_Grid";
-import Notification from "OxzionGUI/Notification";
-
-export { OX_Grid, Notification };
